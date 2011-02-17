97c0b0ceb2b4d918256e474dbeb536af84042673
Hong Cao <hong@ebi.ac.uk>
2011-02-17 12:37:26 +0000

    Creating project structure
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@2 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ src/main/java/uk/ac/ebi/ep/parent/App.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.ep.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
