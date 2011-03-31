c0b7977d6424033b0fd7298e65ce70166e88d029
Hong Cao <hong@ebi.ac.uk>
2011-03-31 15:30:43 +0000

    Create Enzyme service layer interface
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/ep-core@9 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ src/main/java/uk/ac/ebi/ep/core/search/Config.java
@@ -0,0 +1,71 @@
+package uk.ac.ebi.ep.core.search;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import uk.ac.ebi.ep.config.Domain;
+import uk.ac.ebi.ep.config.EnzymeRelatedDomains;
+
+/**
+ *
+ * @since   1.0
+ * @version $LastChangedRevision$ <br/>
+ *          $LastChangedDate$ <br/>
+ *          $Author$
+ * @author  $Author$
+ */
+public class Config {
+
+//********************************* VARIABLES ********************************//
+public static List<Domain> domainList;
+
+//******************************** CONSTRUCTORS ******************************//
+
+
+//****************************** GETTER & SETTER *****************************//
+
+
+//********************************** METHODS *********************************//
+static {
+        try {
+            EnzymeRelatedDomains enzymeRelatedDomains =
+                    unmarshalConfigFile(new File("/Users/hongcao/Projects/enzymeportal/trunk/enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/search/config.xml"));
+            domainList = enzymeRelatedDomains.getDomain();
+        } catch (IOException ex) {
+           //COnfig file not found
+        } catch (JAXBException ex) {
+            //Config file provided can be unmarshalled
+        }
+
+}
+
+    public static EnzymeRelatedDomains unmarshalConfigFile(File inFile)
+             throws IOException, JAXBException
+            {
+        EnzymeRelatedDomains enzymeRelatedDomains = null;
+            JAXBContext jaxbContext = JAXBContext.newInstance("uk.ac.ebi.ep.config");
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JAXBElement  jaxbElement = (JAXBElement) unmarshaller.unmarshal(inFile);
+            enzymeRelatedDomains = (EnzymeRelatedDomains)jaxbElement.getValue();
+        return enzymeRelatedDomains;
+    }
+    
+    public static Domain getDomain(String domainId) {
+        Iterator it = Config.domainList.iterator();
+        Domain domain = null;
+        while (it.hasNext()) {
+            domain = (Domain)it.next();
+            if (domainId.equals(domain.getId())) {
+                return domain;
+            }
+        }
+        return domain;        
+    }
+
+
+}
