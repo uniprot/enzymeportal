364ea1538d7f5336616104b2dccbec5ef99f92a4
Hong Cao <hong@ebi.ac.uk>
2011-04-12 11:51:38 +0000

    Created the search result domain model
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@11 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ ep-domain/src/main/java/uk/ac/ebi/ep/search/parameter/FilterSearchParams.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.ep.search.parameter;
+
+/**
+ *
+ * @since   1.0
+ * @version $LastChangedRevision$ <br/>
+ *          $LastChangedDate$ <br/>
+ *          $Author$
+ * @author  $Author$
+ */
+public class FilterSearchParams extends SearchParams{
+   
+
+}
---
+++ ep-domain/src/main/java/uk/ac/ebi/ep/search/parameter/SearchParams.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.ep.search.parameter;
+
+/**
+ *
+ * @since   1.0
+ * @version $LastChangedRevision$ <br/>
+ *          $LastChangedDate$ <br/>
+ *          $Author$
+ * @author  $Author$
+ */
+public class SearchParams {
+
+    protected String keywords;
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+}
