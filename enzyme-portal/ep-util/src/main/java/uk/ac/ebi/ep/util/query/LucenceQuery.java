bb82c89ee2864e63ab91b2f745e4a1f611e92a78
Hong Cao <hong@ebi.ac.uk>
2011-04-12 12:00:44 +0000

    Changing workflow impl.
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@13 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/ep-util/src/main/java/uk/ac/ebi/ep/util/query/LucenceQuery.java
+++ enzyme-portal/ep-util/src/main/java/uk/ac/ebi/ep/util/query/LucenceQuery.java
@@ -4,21 +4,24 @@ import java.util.Iterator;
 import java.util.List;
 import uk.ac.ebi.ep.config.Domain;
 import uk.ac.ebi.ep.config.SearchField;
+import uk.ac.ebi.ep.search.parameter.SearchParams;
 
 /**
  * Hello world!
  *
  */
 public class LucenceQuery {
+    public static final String ENZYME_FILTER =
+            "EC:(1* OR 2* OR 3* OR 4* OR 5* OR 6* OR 7* OR 8* OR 9*)";
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
-    public static String createQueryOR(Domain domain) {
+    public static String createQueryOR(Domain domain, SearchParams searchParams) {
         StringBuffer query = new StringBuffer();
-        System.out.println(domain.getId());
-        System.out.println(domain.getName());
+        //System.out.println(domain.getId());
+        //System.out.println(domain.getName());
         List<SearchField> SearchFieldList = domain.getSearchFieldList().getSearchField();
         Iterator fieldIt = SearchFieldList.iterator();
         int listLength = SearchFieldList.size();
@@ -27,16 +30,26 @@ public class LucenceQuery {
             SearchField searchField = (SearchField) fieldIt.next();
             query.append(searchField.getId());
             query.append(":");
-            query.append("kinase");
+            query.append(searchParams.getKeywords());
+            query.append("*");
             if (counter <listLength)
             query.append(" OR ");            
             counter++;
         }
             if (domain.getId().equalsIgnoreCase("uniprot")) {
-                query.append(" AND EC:(1* OR 2* OR 3* OR 4* OR 5* OR 6* OR 7* OR 8* OR 9*) ");
+                query.append(" AND " +ENZYME_FILTER);
             }
 
-        System.out.println(query.toString());
+        //System.out.println(query.toString());
+        return query.toString();
+    }
+
+    public static String createQueryToGetEnzymeOnly(String uniprotId){
+        StringBuffer query = new StringBuffer();
+        query.append("id:");
+        query.append(uniprotId);
+        query.append(" AND " );
+        query.append(ENZYME_FILTER);
         return query.toString();
     }
 }
