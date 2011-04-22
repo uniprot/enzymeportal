d63dbfbce9fb944f9e7fcecdce373e2d5a925dcb
Hong Cao <hong@ebi.ac.uk>
2011-04-22 13:23:37 +0000

    Implementing concurrent request invocation - done
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/ep-util@27 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/util/result/EBeyeDataTypeConverter.java
+++ src/main/java/uk/ac/ebi/util/result/EBeyeDataTypeConverter.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import javax.xml.bind.JAXBElement;
+import uk.ac.ebi.ebeye.ResultOfGetReferencedEntriesSet;
 import uk.ac.ebi.ep.config.Domain;
 import uk.ac.ebi.ep.config.ResultField;
-import uk.ac.ebi.webservices.jaxws.stubs.ebeye.ArrayOfArrayOfString;
-import uk.ac.ebi.webservices.jaxws.stubs.ebeye.ArrayOfEntryReferences;
-import uk.ac.ebi.webservices.jaxws.stubs.ebeye.ArrayOfString;
-import uk.ac.ebi.webservices.jaxws.stubs.ebeye.EntryReferences;
+import uk.ac.ebi.webservices.ebeye.ArrayOfArrayOfString;
+import uk.ac.ebi.webservices.ebeye.ArrayOfEntryReferences;
+import uk.ac.ebi.webservices.ebeye.ArrayOfString;
+import uk.ac.ebi.webservices.ebeye.EntryReferences;
 
 /**
  *
@@ -19,63 +20,61 @@ import uk.ac.ebi.webservices.jaxws.stubs.ebeye.EntryReferences;
  *          $Author$
  * @author  $Author$
  */
-public class EBeyeDataTypeConverter {
+public class EBeyeDataTypeConverter extends DataTypeConverter {
 
 //********************************* VARIABLES ********************************//
-
-
 //******************************** CONSTRUCTORS ******************************//
-
-
 //****************************** GETTER & SETTER *****************************//
-
-
 //********************************** METHODS *********************************//
-
     public static ArrayOfString createEbeyeFieldArray(Domain domain) {
         List<ResultField> fieldList = domain.getResultFieldList().getResultField();
         Iterator it = fieldList.iterator();
-        ArrayOfString resultRefFields= new ArrayOfString();
+        ArrayOfString resultRefFields = new ArrayOfString();
         while (it.hasNext()) {
-            ResultField field = (ResultField)it.next();
+            ResultField field = (ResultField) it.next();
             resultRefFields.getString().add(field.getId());
         }
         return resultRefFields;
     }
-    public static String  convertArrayOfStringToString(ArrayOfString arrayOfString) {
+
+    public static String convertArrayOfStringToString(ArrayOfString arrayOfString) {
+        if (arrayOfString == null) {
+            return null;
+        }
         List<String> list = arrayOfString.getString();
         Iterator it = list.iterator();
         StringBuffer sb = new StringBuffer();
         int counter = 0;
         while (it.hasNext()) {
-            sb.append((String)it.next());
+            sb.append((String) it.next());
             counter++;
-            if (counter<list.size()){
+            if (counter < list.size()) {
                 sb.append(",");
             }
         }
         return sb.toString();
     }
 
-    public static List<String> convertArrayOfEntryReferencesToList(ArrayOfEntryReferences
-                        arrayOfEntryReferences) {
+    public static List<String> convertArrayOfEntryReferencesToList(
+                                        ArrayOfEntryReferences arrayOfEntryReferences) {
         List<String> resultList = new ArrayList<String>();
         Iterator it = arrayOfEntryReferences.getEntryReferences().iterator();
         while (it.hasNext()) {
-           EntryReferences entryReferences = (EntryReferences)it.next();
-           JAXBElement<ArrayOfArrayOfString> jAXBElement = entryReferences.getReferences();
+            EntryReferences entryReferences = (EntryReferences) it.next();
+            JAXBElement<ArrayOfArrayOfString> jAXBElement =
+                                                            entryReferences.getReferences();
             resultList.addAll(
                     convertArrayOfArrayOfStringToList(jAXBElement.getValue()));
         }
         return resultList;
     }
 
-    public static List<String> convertArrayOfArrayOfStringToList(ArrayOfArrayOfString
-                        arrayOfArrayOfString) {
+    public static List<String> convertArrayOfArrayOfStringToList(
+                                                ArrayOfArrayOfString arrayOfArrayOfString) {
         List<String> resultList = new ArrayList<String>();
         Iterator it = arrayOfArrayOfString.getArrayOfString().iterator();
         while (it.hasNext()) {
-            ArrayOfString arrayOfString = (ArrayOfString)it.next();
+            ArrayOfString arrayOfString = (ArrayOfString) it.next();
             resultList.addAll(arrayOfString.getString());
         }
         return resultList;
