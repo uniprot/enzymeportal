9a3e8834cc0f45b614edb4336243ef9d0a6056cc
Hong Cao <hong@ebi.ac.uk>
2011-05-31 14:01:17 +0000

    commit before cleaning up
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@60 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- ep-util/src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
+++ ep-util/src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
@@ -1,12 +1,26 @@
 package uk.ac.ebi.ep.util.query;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.SimpleAnalyzer;
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.snowball.SnowballAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.util.Version;
+import uk.ac.ebi.biobabel.lucene.LuceneParser;
 import uk.ac.ebi.ebeye.ParamOfGetResultsIds;
 import uk.ac.ebi.ebeye.ResultOfGetNumberOfResults;
-import uk.ac.ebi.ep.config.Domain;
-import uk.ac.ebi.ep.config.SearchField;
+import uk.ac.ebi.ep.config.jaxb.Domain;
+import uk.ac.ebi.ep.config.jaxb.SearchField;
 import uk.ac.ebi.ep.search.exception.QueryException;
 import uk.ac.ebi.ep.search.parameter.SearchParams;
 import uk.ac.ebi.ep.search.result.Pagination;
@@ -21,10 +35,7 @@ public class LuceneQueryBuilder {
     public static final int EBEYE_MAX_RESULTS_PER_QUERY = 100;
     //133343 results does not pass the load test
     //public static final int MAX_RESULTS = 100000;
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    public static LuceneParser luceneParser = new LuceneParser();  
 
     public static String createQueryOR(Domain domain, SearchParams searchParams) {
         StringBuffer query = new StringBuffer();
@@ -34,13 +45,20 @@ public class LuceneQueryBuilder {
         Iterator fieldIt = SearchFieldList.iterator();
         int listLength = SearchFieldList.size();
         int counter = 1;
+        String keywords = searchParams.getKeywords();
+        int numberOfKeywords = keywords.split("\\s").length;
         query.append("(");
         while (fieldIt.hasNext()) {
             SearchField searchField = (SearchField) fieldIt.next();            
             query.append(searchField.getId());
             query.append(":");
-            query.append(searchParams.getKeywords());
-            query.append("*");
+            if (numberOfKeywords>1) {
+                query.append("\"");
+            }
+            query.append(keywords);
+            if (numberOfKeywords>1) {
+                query.append("\"");
+            }
             if (counter <listLength) {
                 query.append(" OR ");
             }
@@ -49,14 +67,52 @@ public class LuceneQueryBuilder {
             }
             
             counter++;
-        }
+        }        
             if (domain.getId().equalsIgnoreCase("uniprot")) {
+                //query.insert(0, "(");
+                //query.append(")");
                 query.append(" AND " +ENZYME_FILTER);
             }
-
         //System.out.println(query.toString());
         return query.toString();
     }
+
+    /*
+    public static String createQueryOR(Domain domain, SearchParams searchParams) {
+        StringBuffer query = new StringBuffer();
+        //System.out.println(domain.getId());
+        //System.out.println(domain.getName());
+        List<SearchField> SearchFieldList = domain.getSearchFieldList().getSearchField();
+        Iterator fieldIt = SearchFieldList.iterator();
+        int listLength = SearchFieldList.size();
+        int counter = 1;
+        //query.append("(");
+        while (fieldIt.hasNext()) {
+            SearchField searchField = (SearchField) fieldIt.next();
+            query.append(searchField.getId());
+            query.append(":(\"");
+            query.append(searchParams.getKeywords());
+            query.append("\")");
+            if (counter <listLength) {
+                query.append(" OR ");
+            }
+            else {
+                //query.append(")");
+            }
+
+            counter++;
+        }
+        /*
+            if (domain.getId().equalsIgnoreCase("uniprot")) {
+                //query.insert(0, "(");
+                //query.append(")");
+                query.append(" AND " +ENZYME_FILTER);
+            }
+         * 
+         */
+        //System.out.println(query.toString());
+       // return query.toString();
+    //}
 
     public static String createQueryToGetEnzymeOnly(String uniprotId){
         StringBuffer query = new StringBuffer();
@@ -168,5 +224,89 @@ public class LuceneQueryBuilder {
         return paramOfGetResultsIdsList;
     }
 
+
+
+    public static void buildGetAllResultsQuery (String domain) {
+
+
+    }
+
+    /*
+    public static List<String> buildDomainQueryOr(String keywords) {
+        List<Domain> domains = Config.domainList;
+        StringBuffer sb = new StringBuffer();
+        List<String> queryList = new ArrayList();
+        for (Domain domain:domains) {
+            String domainId = domain.getId();
+            List<SearchField> fields = domain.getSearchFieldList().getSearchField();
+            queryList.add(buidFieldsQueryOr(keywords, fields));
+        }
+        return queryList;
+    }
+
+    public static String buidFieldsQueryOr(String keywords, List<SearchField> fields) {
+        String query1 = null;
+        String query2 = null;
+        String cleanKeywords = cleanKeywords(keywords);
+        StringBuffer sb = new StringBuffer();
+        for (SearchField field: fields) {
+            query1 = luceneParser.parseOrTerms(cleanKeywords, field.getId());
+            query2 = luceneParser.parsePhraseTerms(cleanKeywords, field.getId());
+            String query3 = luceneParser.parseUserTerms(cleanKeywords, field.getId());
+            sb.append(query1);
+        }
+        return sb.toString();
+    }
+
+	private static String cleanKeywords(String keywords){
+            StandardAnalyzer standardAnalyzer = new StandardAnalyzer(Version.LUCENE_31);
+           // standardAnalyzer.
+
+            return null;
+        }
+     * 
+     */
+
+   private static final String[] strings = {
+        "cGMP-specific 3',5'-cyclic phosphodiesterase",
+        "H2O7N8 + H2 +"
+    };
+   
+
+    private static final Analyzer[] analyzers = new Analyzer[]{
+        new WhitespaceAnalyzer(Version.LUCENE_31),
+        new SimpleAnalyzer(Version.LUCENE_31),
+        new StopAnalyzer(Version.LUCENE_31),
+        new StandardAnalyzer(Version.LUCENE_31)
+        //new SnowballAnalyzer(Version.LUCENE_31,"test")
+    };
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < strings.length; i++) {
+            analyze(strings[i]);
+        }
+    }
+
+    private static void analyze(String text) throws IOException {
+for (int i = 0; i < analyzers.length; i++) {
+            Analyzer analyzer = analyzers[i];
+        System.out.println("Analzying \"" + text + "\"");        
+            //Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+            System.out.println("\t" + analyzer.getClass().getName() + ":");
+            System.out.print("\t\t");
+
+            TokenStream tokenStream = analyzer.tokenStream("contents", new StringReader(text));
+            OffsetAttribute offsetAttribute = tokenStream.getAttribute(OffsetAttribute.class);
+            CharTermAttribute termAttribute = tokenStream.getAttribute(CharTermAttribute.class);
+
+            while (tokenStream.incrementToken()) {
+                int startOffset = offsetAttribute.startOffset();
+                int endOffset = offsetAttribute.endOffset();
+                String term = termAttribute.toString();
+                System.out.println(term);
+            }
+            
+        }    
+    }
 
 }
