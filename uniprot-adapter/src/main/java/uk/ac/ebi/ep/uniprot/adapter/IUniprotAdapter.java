7ce496e69178e153e0f86b7509072870b20c8ee3
Hong Cao <hong@ebi.ac.uk>
2011-06-01 17:52:56 +0000

    working version without using cache
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@67 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- uniprot-adapter/src/main/java/uk/ac/ebi/ep/uniprot/adapter/IUniprotAdapter.java
+++ uniprot-adapter/src/main/java/uk/ac/ebi/ep/uniprot/adapter/IUniprotAdapter.java
@@ -2,9 +2,8 @@ package uk.ac.ebi.ep.uniprot.adapter;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 import uk.ac.ebi.ep.ebeye.result.jaxb.Result;
+import uk.ac.ebi.ep.search.exception.MultiThreadingException;
 import uk.ac.ebi.ep.search.result.jaxb.EnzymeSummary;
 
 /**
@@ -31,11 +30,11 @@ public interface IUniprotAdapter {
 //********************************** METHODS *********************************//
     public EnzymeSummary getEnzymeEntry(String accession);
 
-    public List<EnzymeSummary> getEnzymeEntries(Set<String> accessionList)
-            throws InterruptedException, ExecutionException, TimeoutException ;
+    public List<EnzymeSummary> getEnzymeEntries(Set<String> accessionList) throws
+            MultiThreadingException;
 
     public List<EnzymeSummary> getEnzymeEntries(List<Result> briefResultList)
-            throws InterruptedException, ExecutionException, TimeoutException ;
+            throws MultiThreadingException;
 
 }
 
