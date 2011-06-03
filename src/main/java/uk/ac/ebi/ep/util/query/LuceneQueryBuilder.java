36a4ce12c5891b28cedc4fff3d32671c88e67bc9
Hong Cao <hong@ebi.ac.uk>
2011-06-03 10:43:49 +0000

    working version without using cache
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/ep-util@71 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
+++ src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
@@ -5,7 +5,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.SimpleAnalyzer;
 import org.apache.lucene.analysis.StopAnalyzer;
