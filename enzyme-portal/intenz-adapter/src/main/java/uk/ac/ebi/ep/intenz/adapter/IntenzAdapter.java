4191e9642cf4ac0089de2b697f8e278fab3c509a
Hong Cao <hong@ebi.ac.uk>
2011-08-08 09:12:10 +0000

    Implementation of enzyme tab: function, classification , synonyms -> done
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@139 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/intenz-adapter/src/main/java/uk/ac/ebi/ep/intenz/adapter/IntenzAdapter.java
+++ enzyme-portal/intenz-adapter/src/main/java/uk/ac/ebi/ep/intenz/adapter/IntenzAdapter.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.ep.intenz.adapter;
 
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -8,9 +9,13 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import uk.ac.ebi.ep.enzyme.model.EcClass;
+import uk.ac.ebi.ep.enzyme.model.Enzyme;
+import uk.ac.ebi.ep.enzyme.model.EnzymeModel;
 import uk.ac.ebi.ep.intenz.adapter.IntenzCallable.GetSynonymsCaller;
 import uk.ac.ebi.ep.search.exception.MultiThreadingException;
 import uk.ac.ebi.intenz.adapter.util.IntenzUtil;
+import uk.ac.ebi.intenz.xml.jaxb.Intenz;
 
 /**
  *
@@ -37,22 +42,49 @@ public class IntenzAdapter implements IintenzAdapter{
     public Map<String, Set<String>> getSynonyms(
             Set<String> ecNumbers) throws MultiThreadingException {
         ExecutorService pool =  pool = Executors.newCachedThreadPool();
-        Map<String, Set<String>> resultMap = new Hashtable<String, Set<String>>();
-        for (String ecNumber:ecNumbers) {
-            Callable<Set<String>> callable = new GetSynonymsCaller(
-                    IntenzUtil.createIntenzEntryUrl(ecNumber));
-            Future<Set<String>>future = pool.submit(callable);
-            try {
-                Set<String> synonyms = (Set<String>) future.get();
-                if (synonyms.size()>0) {
-                    resultMap.put(ecNumber, (Set<String>) future.get());
+        Map<String, Set<String>> resultMap = null;
+        try {
+            resultMap = new Hashtable<String, Set<String>>();
+            for (String ecNumber:ecNumbers) {
+                Callable<Set<String>> callable = new GetSynonymsCaller(
+                        IntenzUtil.createIntenzEntryUrl(ecNumber));
+                Future<Set<String>>future = pool.submit(callable);
+                try {
+                    Set<String> synonyms = (Set<String>) future.get();
+                    if (synonyms.size()>0) {
+                        resultMap.put(ecNumber, (Set<String>) future.get());
+                    }
+                } catch (InterruptedException ex) {
+                    throw new MultiThreadingException(IintenzAdapter.FAILED_MSG, ex);
+                } catch (ExecutionException ex) {
+                    throw new MultiThreadingException(IintenzAdapter.FAILED_MSG, ex);
                 }
-            } catch (InterruptedException ex) {
-                throw new MultiThreadingException(IintenzAdapter.FAILED_MSG, ex);
-            } catch (ExecutionException ex) {
-                throw new MultiThreadingException(IintenzAdapter.FAILED_MSG, ex);
             }
         }
+        finally {
+            pool.shutdown();
+        }
         return resultMap;
+    }
+
+    public EnzymeModel getEcDetails(EnzymeModel enzymeModel) {
+        String ec = enzymeModel.getEc().get(0);
+        GetSynonymsCaller callable = new GetSynonymsCaller(
+                 IntenzUtil.createIntenzEntryUrl(ec));
+        Intenz intenz = callable.getData();
+        Set<String> synonym = callable.getSynonyms(intenz);
+        if (synonym.size() > 0) {
+            enzymeModel.getSynonym().addAll(synonym);
+        }
+
+        List<EcClass> ecClassList = callable.getEcClass(intenz);
+
+        Enzyme enzyme = new Enzyme();
+        if (ecClassList.size() > 0) {
+            enzyme.setEcclasshierarchy(ecClassList);
+        }
+        enzymeModel.setEnzyme(enzyme);
+
+        return enzymeModel;
     }
 }
