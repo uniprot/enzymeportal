5bd53d6937a4edd785694fa986068a8ae4e00e7c
Hong Cao <hong@ebi.ac.uk>
2011-08-08 09:12:28 +0000

    Implementation of enzyme tab: function, classification , synonyms -> done
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/ep-util@140 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
+++ src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
@@ -22,8 +22,8 @@ public class LuceneQueryBuilder {
     public static final String ENZYME_FILTER =
             "EC:(1* OR 2* OR 3* OR 4* OR 5* OR 6* OR 7*)";
     public static final String UNIPROT_ID_FIELD ="id";
-    public static final String UNIPROT_NAME_FIELD ="name";
-    public static final String ACCESSION_FIELD ="acc";
+    //public static final String UNIPROT_NAME_FIELD ="name";
+    //public static final String ACCESSION_FIELD ="acc";
     public static final String LUCENE_WILDCARD ="_*";
     public static final String ENZYME_FILTER_UNIPROTAPI ="ec:*.*";
     public static final String UNIPROT_SPECIES_FIELD ="organism";
@@ -105,6 +105,15 @@ public class LuceneQueryBuilder {
         return query.toString();
     }
 
+    public static String createFieldValueQuery(String field, String value) {
+        StringBuffer query = new StringBuffer();
+        query.append(field);
+        query.append(":");
+        query.append("\"");
+        query.append(value);
+        query.append("\"");
+        return query.toString();
+    }
 
     public static String addSpeciesFilterQuery(String simpleQuery
             , String speciesField, Collection<String> speciesList) {
@@ -162,7 +171,10 @@ public class LuceneQueryBuilder {
     public static List<String> createUniprotAPIQueryByIdPrefixes(
             List<String> idPrefixes, Collection<String> speciesFilter) {
         List<String> queryList = new ArrayList<String>();
-        List<String> escapedSpeciesFilter = escapeSpecies(speciesFilter);
+        List<String> escapedSpeciesFilter = null;
+        if (speciesFilter != null) {
+            escapedSpeciesFilter = escapeSpecies(speciesFilter);
+        }
         for (String idPrefix : idPrefixes) {
             StringBuffer sb = new StringBuffer();
             sb.append(UNIPROT_ID_FIELD);
@@ -180,7 +192,7 @@ public class LuceneQueryBuilder {
         }
         return queryList;
     }
-
+ 
     public static String createQueryIN(
             String fieldName, boolean wildcard, Collection<String> fieldValues) {
         StringBuffer query = new StringBuffer();
@@ -202,16 +214,6 @@ public class LuceneQueryBuilder {
         return query.toString();
 
     }
-    public static String createAccessionQueryIN(Collection<String> accs) {
-        return createQueryIN(ACCESSION_FIELD, false, accs);
-    }
-
-    public static String createUniprotQueryForEnzyme(Collection<String> accs, Collection<String> speciesFilter) {
-        String query = createAccessionQueryIN(accs) + " AND " + ENZYME_FILTER;
-        return addSpeciesFilterQuery(
-                    query, EBEYE_SPECIES_FIELD, speciesFilter);
-    }
-
 
     /**
      * Create a list of Lucene queries IN (eg.: id:("PDE7B_HUMAN","PDE7B_MOUSE"))
