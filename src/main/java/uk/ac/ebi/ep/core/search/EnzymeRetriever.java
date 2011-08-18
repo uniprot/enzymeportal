12a094b31000b95b85eca96865e71a0632a45839
Hong Cao <hong@ebi.ac.uk>
2011-08-18 20:29:32 +0000

    Implementation of Reactions and Pathways tab: Reaction --> done
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/ep-core@181 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/core/search/EnzymeRetriever.java
+++ src/main/java/uk/ac/ebi/ep/core/search/EnzymeRetriever.java
@@ -2,18 +2,29 @@ package uk.ac.ebi.ep.core.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uk.ac.ebi.ep.entry.exception.EnzymeRetrieverException;
 import uk.ac.ebi.ep.enzyme.model.EnzymeModel;
 import uk.ac.ebi.ep.enzyme.model.EnzymeReaction;
+import uk.ac.ebi.ep.enzyme.model.Equation;
 import uk.ac.ebi.ep.enzyme.model.ReactionPathway;
 import uk.ac.ebi.ep.search.exception.MultiThreadingException;
 import uk.ac.ebi.ep.util.query.LuceneQueryBuilder;
+import uk.ac.ebi.rhea.domain.Database;
 import uk.ac.ebi.rhea.ws.client.IRheaAdapter;
 import uk.ac.ebi.rhea.ws.client.RheaFetchDataException;
 import uk.ac.ebi.rhea.ws.client.RheasResourceClient;
 import uk.ac.ebi.rhea.ws.response.cmlreact.Link;
 import uk.ac.ebi.rhea.ws.response.cmlreact.Map;
+import uk.ac.ebi.rhea.ws.response.cmlreact.Molecule;
+import uk.ac.ebi.rhea.ws.response.cmlreact.Product;
+import uk.ac.ebi.rhea.ws.response.cmlreact.ProductList;
+import uk.ac.ebi.rhea.ws.response.cmlreact.Reactant;
+import uk.ac.ebi.rhea.ws.response.cmlreact.ReactantList;
 import uk.ac.ebi.rhea.ws.response.cmlreact.Reaction;
+import uk.ac.ebi.xchars.SpecialCharacters;
+import uk.ac.ebi.xchars.domain.EncodingType;
 
 /**
  *
@@ -27,11 +38,9 @@ public class EnzymeRetriever extends EnzymeFinder implements IEnzymeRetriever {
 
 //********************************* VARIABLES ********************************//
     protected String accesion;
-
     protected IRheaAdapter rheaAdapter;
 
     //protected IReactomeAdapter reactomeAdapter;
-
     public String getAccesion() {
         return accesion;
     }
@@ -40,26 +49,20 @@ public class EnzymeRetriever extends EnzymeFinder implements IEnzymeRetriever {
         this.accesion = accesion;
     }
 
-
 //******************************** CONSTRUCTORS ******************************//
-
     public EnzymeRetriever() {
         rheaAdapter = new RheasResourceClient();
         //reactomeAdapter = new ReactomeAdapter();
     }
 
-
 //****************************** GETTER & SETTER *****************************//
-
-
 //********************************** METHODS *********************************//
-
     public EnzymeModel getEnzyme(String uniprotAccession) throws EnzymeRetrieverException {
-        EnzymeModel enzymeModel = (EnzymeModel)this.uniprotAdapter.getEnzymeSummary(uniprotAccession);
+        EnzymeModel enzymeModel = (EnzymeModel) this.uniprotAdapter.getEnzymeSummary(uniprotAccession);
         try {
             this.intenzAdapter.getEnzymeDetails(enzymeModel);
         } catch (MultiThreadingException ex) {
-            throw new EnzymeRetrieverException ("Unable to retrieve the entry details! ", ex);
+            throw new EnzymeRetrieverException("Unable to retrieve the entry details! ", ex);
         }
         return enzymeModel;
     }
@@ -72,59 +75,196 @@ public class EnzymeRetriever extends EnzymeFinder implements IEnzymeRetriever {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public EnzymeModel getReactionsPathways(String uniprotAccession) throws EnzymeRetrieverException {
-        EnzymeModel enzymeModel = this.getEnzyme(uniprotAccession);
-        try {                        
-            List<String> ecList = enzymeModel.getEc();
-            String query = LuceneQueryBuilder.createQueryIN(IRheaAdapter.RheaQueryFields.EC.name(), false, ecList);
-            List<Reaction> reactions =  this.rheaAdapter.getRheasInCmlreact(query);
-            List<ReactionPathway> reactionPathways = new ArrayList<ReactionPathway>();
-            for (uk.ac.ebi.rhea.ws.response.cmlreact.Reaction reaction:reactions) {
-                ReactionPathway reactionPathway = new ReactionPathway();
-                getReactomeRef(reaction);
-                EnzymeReaction epReaction = new EnzymeReaction();
-                epReaction.setId(reaction.getId());
-                reactionPathways.add(reactionPathway);
-                //reactionPathway.getReactions().add(epReaction);
-                //epReaction.setTitle();
-            }
-            //enzymeModel.
+    public EnzymeModel getReactionsPathways(EnzymeModel enzymeModel) throws EnzymeRetrieverException {
+        List<ReactionPathway> reactionPathways = new ArrayList<ReactionPathway>();
+        List<String> ecList = enzymeModel.getEc();
+        String query = LuceneQueryBuilder.createQueryIN(IRheaAdapter.RheaQueryFields.EC.name(), false, ecList);
+        List<Reaction> reactions;
+        try {
+            reactions = this.rheaAdapter.getRheasInCmlreact(query);
         } catch (RheaFetchDataException ex) {
             throw new EnzymeRetrieverException("Query data from Rhea failed! ", ex);
         }
+        for (Reaction reaction : reactions) {
+            ReactionPathway reactionPathway = mapReactionToReactionPathway(reaction);
+            reactionPathways.add(reactionPathway);
+        }
+        enzymeModel.setReactionpathway(reactionPathways);
+        return enzymeModel;
+
+    }
+
+    public EnzymeModel getReactionsPathways(String uniprotAccession) throws EnzymeRetrieverException {
+        EnzymeModel enzymeModel = this.getEnzyme(uniprotAccession);
+        getReactionsPathways(enzymeModel);
         return enzymeModel;
     }
 
-    public List<String> getReactomeRef(Reaction reaction) {
-        List<String> accessions = new ArrayList<String>();
-        List<Object> reactionElements = reaction.getReactiveCentreAndMechanismAndReactantList();
-        Map xRefs = getRheaXrefs(reactionElements);
-        if (xRefs != null) {
-            List<Link> linkList = xRefs.getLink();
-            if (linkList != null) {
-                accessions.addAll(getAccessionList(linkList, "rctm"));
-            }
-        }
-        return accessions;
+    public void createReactionTitle(Reaction reaction) {
     }
+    /*
+    public List<String> getReactomeRef(Reaction reaction) {
+    List<String> accessions = new ArrayList<String>();
+    List<Object> reactionElements = reaction.getReactiveCentreAndMechanismAndReactantList();
+    Map xRefs = getRheaXrefs(reactionElements);
+    if (xRefs != null) {
+    List<Link> linkList = xRefs.getLink();
+    if (linkList != null) {
+    accessions.addAll(getAccessionList(linkList, "rctm"));
+    }
+    }
+    return accessions;
+    }
+     */
 
     public void getReactionDescription(List<String> reactomeAccessions) {
-
     }
-    public Map getRheaXrefs(List<Object> reactionElements) {
-        Map xRefs = null;
-        for (Object reactionElement: reactionElements) {
-            if (reactionElement instanceof Map) {
-                xRefs = (Map)reactionElement;
-                break;
+    /*
+    public List<Link> getRheaXrefs(List<Object> xRefs) {
+    List<Link> linkList = new ArrayList<Link>();
+    for (Object xRef: xRefs) {
+    linkList.add((Link)xRefs);
+    }
+    return linkList;
+    }
+     *
+     */
+
+
+    public List<Object> getLinks(String dbCode,List<Link> xRefs) {
+        List<Object> links = new ArrayList<Object>();
+        for (Link link: xRefs) {
+            String idPrefix = link.getId();
+            String dbCodeInLink = idPrefix.split(":")[0];
+            if (dbCodeInLink.equalsIgnoreCase(dbCode)) {
+                links.add(link);
             }
         }
-        return xRefs;
+        return links;
+    }
+
+    public List<Object> getMechanismLinks(List<Link> xRefs) {
+        List<Object> links = new ArrayList<Object>();
+        String macieDbCode = Database.MACIE.getDbCode();
+        return getLinks(macieDbCode, xRefs);
+    }
+
+        public List<Object> getPathwayLinks(List<Link> xRefs) {
+        List<Object> links = new ArrayList<Object>();
+        String reactomeDbCode = Database.REACTOME.getDbCode();
+        return getLinks(reactomeDbCode, xRefs);
+    }
+    public ReactionPathway mapReactionToReactionPathway(Reaction reaction) {
+        List<Object> reactionElements = reaction.getReactiveCentreAndMechanismAndReactantList();
+        ReactionPathway reactionPathway = new ReactionPathway();
+        EnzymeReaction enzymeReaction = new EnzymeReaction();
+        reactionElements.get(0);
+        Equation equation = new Equation();
+        equation.setDirection(createEquationSymbol(reaction));
+        List<Link> linkList = new ArrayList<Link>();
+        for (Object reactionElement : reactionElements) {
+            if (reactionElement instanceof Map) {
+                Map xRefs = (Map) reactionElement;
+                linkList.addAll(xRefs.getLink());
+            }
+            if (reactionElement instanceof ReactantList) {
+                ReactantList reactantList = (ReactantList) reactionElement;
+                List<Object> molObjList = reactantList.getReactantListOrReactant();
+                List<Molecule> reactantMolList = getMolecules(molObjList);
+                equation.getReactantlist().addAll(reactantMolList);
+            }
+            if (reactionElement instanceof ProductList) {
+                ProductList productList = (ProductList) reactionElement;
+                List<Object> molObjList = productList.getProductListOrProduct();
+                List<Molecule> productMolList = getMolecules(molObjList);
+                equation.getProductlist().addAll(productMolList);
+
+            }
+        }
+        //Set reaction properties
+        String rheaId = reaction.getId();
+        enzymeReaction.setId(rheaId);
+        String reactionTitle = createReactionTitle(equation);
+        enzymeReaction.setTitle(reactionTitle);
+        enzymeReaction.setEquation(equation);
+        reactionPathway.setReaction(enzymeReaction);
+        List<Object> mecList = getMechanismLinks(linkList);
+        reactionPathway.setMechanism(mecList);
+        List<Object> pwList = getPathwayLinks(linkList);
+        reactionPathway.setPathways(pwList);
+        return reactionPathway;
+    }
+
+    public String createReactionTitle(Equation equation) {
+        StringBuffer sb = new StringBuffer();
+        List<Object> reacanttList = equation.getReactantlist();
+        String reactantsString = createParticipantString(reacanttList);
+        sb.append(reactantsString);
+        sb.append(equation.getDirection());
+        sb.append(createParticipantString(equation.getProductlist()));
+        return sb.toString();
+    }
+
+    public String createEquationSymbol(Reaction reaction) {
+        String rheaDirectionConv = reaction.getConvention();
+        String direction = rheaDirectionConv.split(":")[1];
+        return createEquationSymbol(direction);
+    }
+
+    public String createEquationSymbol(String direction) {
+        String symbol = null;
+        if (direction.equalsIgnoreCase("direction.UN")) {
+            symbol = " <?> ";
+        }
+        if (direction.equalsIgnoreCase("direction.BI")) {
+            symbol = " <=> ";
+        }
+        if (direction.equalsIgnoreCase("direction.LR")) {
+            symbol = " => ";
+        }
+        if (direction.equalsIgnoreCase("direction.RL")) {
+            symbol = " <= ";
+        }
+
+        return symbol;
+    }
+
+    public String createParticipantString(List<Object> molList) {
+        StringBuffer sb = new StringBuffer();
+        SpecialCharacters xchars = SpecialCharacters.getInstance(null);
+        int counter = 1;
+        for (Object molObj : molList) {
+            Molecule molecule = (Molecule) molObj;
+            String molName = molecule.getTitle();
+            String chemMolName = xchars.xml2Display(molName);
+            sb.append(chemMolName);
+            if (counter < molList.size()) {
+                sb.append(" + ");
+            }
+            counter++;
+        }
+        return sb.toString();
+    }
+
+    public List<Molecule> getMolecules(List<Object> molObjList) {
+        List<Molecule> molList = new ArrayList<Molecule>();
+        for (Object molObject : molObjList) {
+            Molecule molecule = null;
+            if (molObject instanceof Reactant) {
+                Reactant reactant = (Reactant) molObject;
+                molecule = reactant.getMolecule();
+            } else {
+                Product product = (Product) molObject;
+                molecule = product.getMolecule();
+            }
+            molList.add(molecule);
+        }
+        return molList;
     }
 
     public List<String> getAccessionList(List<Link> linkList, String domainPrefix) {
         List<String> accessionList = new ArrayList<String>();
-        for (Link link:linkList) {
+        for (Link link : linkList) {
             String id = link.getId();
             String[] idFragments = id.split(":");
             String prefix = idFragments[0];
@@ -152,5 +292,4 @@ public class EnzymeRetriever extends EnzymeFinder implements IEnzymeRetriever {
     public EnzymeModel getLiterarture(String uniprotAccession) throws EnzymeRetrieverException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
 }
