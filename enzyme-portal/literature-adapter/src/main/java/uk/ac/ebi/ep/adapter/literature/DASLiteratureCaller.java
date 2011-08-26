371da303805179f4c494b532ec0ffd9c34e81635
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-08-26 12:05:03 +0000

    Preliminary implementation of literature adapter
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@266 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ enzyme-portal/literature-adapter/src/main/java/uk/ac/ebi/ep/adapter/literature/DASLiteratureCaller.java
@@ -0,0 +1,147 @@
+package uk.ac.ebi.ep.adapter.literature;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.ebi.biobabel.citations.CitexploreWSClient;
+import uk.ac.ebi.biobabel.citations.DataSource;
+import uk.ac.ebi.cdb.webservice.Citation;
+import uk.ac.ebi.cdb.webservice.Journal;
+import uk.ac.ebi.cdb.webservice.JournalIssue;
+import uk.ac.ebi.cdb.webservice.QueryException_Exception;
+import uk.ac.ebi.das.jdas.adapters.features.DasGFFAdapter.SegmentAdapter;
+import uk.ac.ebi.das.jdas.adapters.features.FeatureAdapter;
+import uk.ac.ebi.das.jdas.exceptions.ValidationException;
+import uk.ac.ebi.ep.adapter.das.SimpleDASFeaturesCaller;
+
+/**
+ * Class to get literature from DAS servers, based on UniProt IDs.
+ * <br>
+ * <b>NOTE:</b> the citations will be retrieved from CiteXplore by PubMed ID.
+ * In case of not having a PubMed ID or if there is any problem with CiteXplore,
+ * they won't contain information about authors nor abstract, as these are not
+ * provided by DAS GFF.
+ * @author rafa
+ *
+ */
+public class DASLiteratureCaller implements Callable<Set<Citation>> {
+
+	private static Logger LOGGER = Logger.getLogger(DASLiteratureCaller.class);
+	
+	private SimpleDASFeaturesCaller featuresCaller;
+	
+	public DASLiteratureCaller(String serverURL, String segment){
+		featuresCaller = new SimpleDASFeaturesCaller(serverURL, segment);
+	}
+	
+	public Set<Citation> call() throws Exception {
+		return getLiterature(featuresCaller.call());
+	}
+
+	/**
+	 * Extracts literature information from the segments returned by DAS.
+	 * @param segments
+	 * @return
+	 */
+	protected Set<Citation> getLiterature(List<SegmentAdapter> segments) {
+		Set<Citation> citations = null;
+		for (SegmentAdapter segment : segments) {
+			try {
+				for (FeatureAdapter feature : segment.getFeature()) {
+					if (feature.getType().getId().equals("summary")
+							&& feature.getLabel().equals("Primary Citation")){
+						Citation citation = null;
+						String pubMedId = getPubMedId(feature.getLinks().get(0).getHref());
+						if (pubMedId != null && pubMedId.length() > 0){
+							// Try to get from CiteXplore with the PubMed ID:
+							citation = getCitationFromCitexplore(pubMedId);
+						}
+						if (citation == null){
+							// Build a citation with data from DAS:
+							citation = buildCitation(feature, pubMedId);
+						}
+						if (citations == null) citations = new HashSet<Citation>();
+						citations.add(citation);
+					}
+				}
+			} catch (ValidationException e) {
+				LOGGER.error("Unable to get citations from DAS for " + segment.getId(), e);
+			}
+		}
+		return citations;
+	}
+
+	private Citation buildCitation(FeatureAdapter feature, String pubMedId) {
+		Citation citation;
+		citation = new Citation();
+		JournalIssue issue = new JournalIssue();
+		Journal journal = new Journal();
+		issue.setJournal(journal);
+		citation.setJournalIssue(issue);
+		citation.setTitle(feature.getNotes().get(0));
+		parseJournal(feature.getNotes().get(1), citation);
+		//parseAuthors(feature.getNotes().get(2), citation);
+		citation.setDataSource(DataSource.MED.name());
+		citation.setExternalId(pubMedId);
+		return citation;
+	}
+
+	protected Citation getCitationFromCitexplore(String pubMedId) {
+		Citation citation = null;
+		try {
+			citation = CitexploreWSClient.getCitation(DataSource.MED, pubMedId);
+		} catch (QueryException_Exception e) {
+			LOGGER.error("Unable to get citation from CiteXplore", e);
+		}
+		return citation;
+	}
+
+	/**
+	 * Extracts the PubMed ID from the URL returned by DAS.
+	 * @param url
+	 * @return a PubMed ID
+	 */
+	protected String getPubMedId(String url) {
+		final String pubMedParam = "Citation_pubmed_id=";
+		return url.substring(
+				url.lastIndexOf(pubMedParam) + pubMedParam.length());
+	}
+
+	/* No authors from DAS' GFF!!
+	protected void parseAuthors(String authors, Citation citation){
+		for (String sAuthor : authors.split(",")) {
+			Author author = new Author();
+			author.setFullName(sAuthor);
+			citation.getAuthorCollection().add(author);
+		}
+	}
+	*/
+
+	/**
+	 * Populates a Citation with journal data from a string.
+	 * @param journal String with the standard notation of the publication,
+	 * 		which comes from DAS as
+	 * 		<code>[journal] vol:[volume] page:[pages] ([year])</code>
+	 * @param citation A CiteXplore citation.
+	 */
+	protected void parseJournal(String journal, Citation citation) {
+		Pattern p = Pattern.compile("(.+) vol:(\\d+) page:(\\d+-\\d+) \\((\\d{4})\\)");
+		Matcher m = p.matcher(journal);
+		if (m.matches()){
+			citation.getJournalIssue().getJournal().setTitle(m.group(1));
+			citation.getJournalIssue().setVolume(m.group(2));
+			citation.setPageInfo(m.group(3));
+			citation.getJournalIssue().setYearOfPublication(
+					Short.valueOf(m.group(4)));
+		} else {
+			LOGGER.error("Format not recognised for DAS citation: " + journal);
+		}
+	}
+
+}
--- enzyme-portal/literature-adapter/src/main/java/uk/ac/ebi/ep/adapter/literature/SimpleLiteratureAdapter.java
+++ enzyme-portal/literature-adapter/src/main/java/uk/ac/ebi/ep/adapter/literature/SimpleLiteratureAdapter.java
@@ -1,12 +1,183 @@
 package uk.ac.ebi.ep.adapter.literature;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
 import uk.ac.ebi.cdb.webservice.Citation;
+import uk.ac.ebi.ep.adapter.das.IDASFeaturesAdapter;
 
 public class SimpleLiteratureAdapter implements ILiteratureAdapter {
+	
+	private static final Logger LOGGER = Logger.getLogger(SimpleLiteratureAdapter.class);
+	
+	public enum CitationLabel { // FIXME take strings from common properties file
+		enzyme("Enzyme"),
+		proteinStructure("Protein structure");
+		private String label;
+		private CitationLabel(String label){
+			this.label = label;
+		}
+		public String toString(){ return label; }
+	}
+	
+	/**
+	 * Wrapper around a CiteXplore citation which implements
+	 * {@link #equals(Object)} and {@link #hashCode()} methods, and
+	 * labels it according to the source it came from.
+	 * @author rafa
+	 */
+	public class LabelledCitation {
+		private Citation citation;
+		private EnumSet<CitationLabel> labels;
+		public LabelledCitation(Citation citation, CitationLabel label){
+			this.citation = citation;
+			this.labels = EnumSet.of(label);
+		}
+		public Citation getCitation() {
+			return citation;
+		}
+		public EnumSet<CitationLabel> getLabel() {
+			return labels;
+		}
+		public void addLabel(CitationLabel label) {
+			this.labels.add(label);
+		}
+		/**
+		 * Equals method based on bibliography identifier, namely PubMed ID.
+		 * It does not take labels into account.
+		 */
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof LabelledCitation)) return false;
+			if (o == this) return true;
+			LabelledCitation other = (LabelledCitation) o;
+			return other.citation.getDataSource().equals(this.citation.getDataSource())
+					&& other.citation.getExternalId().equals(this.citation.getExternalId());
+		}
+		@Override
+		public int hashCode() {
+			int hash = 17;
+			hash = hash * 17 + citation.getDataSource().hashCode();
+			hash = hash * 17 + citation.getExternalId().hashCode();
+			return hash;
+		}
+		
+	}
+	
+	private ExecutorService threadPool = Executors.newCachedThreadPool();
+	
+	// Not used, it does not find UniProt IDs easily
+	//private CitexploreLiteratureCaller citexploreCaller;
+	
+	public List<LabelledCitation> getCitations(String uniprotId) {
+		List<LabelledCitation> citations = null;
+		List<Callable<Set<Citation>>> callables = getCallables(uniprotId);
+		List<Future<Set<Citation>>> futures = null;
+		try {
+			futures = threadPool.invokeAll(callables);
+			// Collect results:
+			citations = new ArrayList<LabelledCitation>();
+			for (Future<Set<Citation>> future : futures) {
+				try {
+					Set<Citation> cits = future.get();
+					CitationLabel label = getLabel(callables.get(futures.indexOf(future)));
+					for (Citation cit : cits) {
+						LabelledCitation citation = new LabelledCitation(cit, label);
+						if (citations.contains(citation)){
+							citations.get(citations.indexOf(citation)).addLabel(label);
+						} else {
+							citations.add(citation);
+						}
+					}
+				} catch (InterruptedException e) {
+					LOGGER.error(getLabel(callables.get(futures.indexOf(future))), e);
+				} catch (ExecutionException e) {
+					LOGGER.error(getLabel(callables.get(futures.indexOf(future))), e);
+				}
+			}		
+		} catch (InterruptedException e) {
+			LOGGER.error("Unable to send requests for citations", e);
+		} finally {
+			List<Runnable> notExecuted = threadPool.shutdownNow();
+			if (!notExecuted.isEmpty()){
+				LOGGER.error(notExecuted.size() + " jobs not sent!");
+			}
+		}
+//		futures = new ArrayList<Future<Set<Citation>>>();
+//		for (Callable<Set<Citation>> callable : callables) {
+//			futures.add(threadPool.submit(callable));
+//		}
+//		List<Future<Set<Citation>>> pending =
+//				new ArrayList<Future<Set<Citation>>>(futures);
+//		// Collect results:
+//		Set<LabelledCitation> citations = new HashSet<LabelledCitation>();
+//		while (!pending.isEmpty()){
+//			for (Future<Set<Citation>> future : pending) {
+//				if (future.isDone()){
+//					try {
+//						Set<Citation> cits = future.get();
+//						CitationLabel label = getLabel(callables.get(futures.indexOf(future)));
+//						for (Citation cit : cits) {
+//							LabelledCitation citation = new LabelledCitation(cit, label);
+//							if (citations.contains(citation)){
+//								
+//							} else {
+//								
+//							}
+//							citations.add();
+//						}
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					} catch (ExecutionException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					} finally {
+//						
+//					}
+//				}
+//			}
+//		}
+		return citations;
+	}
 
-	public Citation getCitations(String uniprotId) {
-		// TODO Auto-generated method stub
-		return null;
+	private List<Callable<Set<Citation>>> getCallables(String uniprotId) {
+		List<Callable<Set<Citation>>> callables =
+				new ArrayList<Callable<Set<Citation>>>();
+		callables.add(new UniprotJapiLiteratureCaller(uniprotId));
+		callables.add(new DASLiteratureCaller(IDASFeaturesAdapter.PDBE_DAS_URL, uniprotId));
+		return callables;
+	}
+	
+	/* FIXME: odd job! */
+	private CitationLabel getLabel(Callable<Set<Citation>> callable){
+		CitationLabel label = null;
+		if (callable.getClass().equals(UniprotJapiLiteratureCaller.class)){
+			label = CitationLabel.enzyme;
+		} else if (callable.getClass().equals(DASLiteratureCaller.class)){
+			label = CitationLabel.proteinStructure;
+		}
+		return label;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		if (threadPool != null){
+			threadPool.shutdownNow();
+			if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)){
+				LOGGER.error("Thread pool did not terminate");
+			}
+		}
 	}
 
 }
