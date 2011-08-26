425ff61e17b38100f15ef8adc84c7014c4c68b7d
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-08-26 12:03:28 +0000

    DAS server URL can be set now
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@264 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/das-adapter/src/main/java/uk/ac/ebi/ep/adapter/das/SimpleDASFeaturesAdapter.java
+++ enzyme-portal/das-adapter/src/main/java/uk/ac/ebi/ep/adapter/das/SimpleDASFeaturesAdapter.java
@@ -1,20 +1,54 @@
 package uk.ac.ebi.ep.adapter.das;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 import javax.xml.bind.JAXBException;
 
 import uk.ac.ebi.das.jdas.adapters.features.DasGFFAdapter.SegmentAdapter;
 
 public class SimpleDASFeaturesAdapter implements IDASFeaturesAdapter {
-
+	
+	private String dasURL;
+	
+	@Deprecated
+	public SimpleDASFeaturesAdapter(){
+		dasURL = IDASFeaturesAdapter.PDBE_DAS_URL;
+	}
+	
+	public SimpleDASFeaturesAdapter(String dasUrl)
+	throws MalformedURLException, IOException{
+		setDasUrl(dasUrl);
+	}
+	
+	/**
+	 * Setter for the dasUrl field.
+	 * @param dasURL
+	 * @throws MalformedURLException if the URL is bad
+	 * @throws IOException in case of problem connecting to that URL
+	 */
+	public void setDasUrl(String dasURL)
+	throws MalformedURLException, IOException{
+		if (dasURL == null){
+			throw new NullPointerException("dasURL cannot be null");
+		} else {
+			URL url = new URL(dasURL);
+			URLConnection con = url.openConnection();
+			con.connect();
+			con.getContent();
+			// end of checks
+			this.dasURL = dasURL;
+		}
+	}
+	
 	public SegmentAdapter getSegment(String segmentId)
 	throws MalformedURLException, JAXBException {
 		SimpleDASFeaturesCaller callable =
-				new SimpleDASFeaturesCaller(IDASFeaturesAdapter.PDBE_DAS_URL, segmentId);
+				new SimpleDASFeaturesCaller(dasURL, segmentId);
 		List<SegmentAdapter> adapters = callable.getSegments();
 		return adapters == null || adapters.size() == 0?
 				null :
@@ -25,7 +59,7 @@ public class SimpleDASFeaturesAdapter implements IDASFeaturesAdapter {
 	throws MalformedURLException, JAXBException {
 		// No need of multithreading, as we can request all of them at once:
 		SimpleDASFeaturesCaller callable =
-				new SimpleDASFeaturesCaller(IDASFeaturesAdapter.PDBE_DAS_URL, segmentIds);
+				new SimpleDASFeaturesCaller(dasURL, segmentIds);
 		return callable.getSegments();
 	}
 
