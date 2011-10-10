711e9db842ddbd55894f7987c7aabb765ef606d6
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-10 11:59:01 +0000

    Moved from ep-web-client module
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@324 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ ep-domain/src/main/java/uk/ac/ebi/ep/entry/Fields.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.ep.entry;
+
+/**
+ * Fields to show for an enzyme in the portal.
+ * @author rafa
+ *
+ */
+public enum Fields {
+    enzyme,
+    proteinStructure,
+    reactionsPathways,
+    molecules,
+    diseaseDrugs,
+    literature,
+    brief,
+    full
+}
