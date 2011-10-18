8456efd2c00ba228d764af6fbe802547c658f4c1
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-18 16:00:08 +0000

    fixed command syntax
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@347 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/uniprotBsub.sh
+++ enzyme-portal/mega-mapper/scripts/uniprotBsub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BSUB_OPTS='-R "rusage[mem=32000]" -M 32000 -q production-rh6'
-bsub $BSUB_OPTS $(dirname $0)/uniprot.sh
+bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 \
+    $(dirname $0)/uniprot.sh
