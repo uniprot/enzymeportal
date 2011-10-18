ea156ff65b983d284f1ac7b6695377ee23f7a5c5
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-18 15:44:56 +0000

    Files are not compressed.
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@345 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- mega-mapper/scripts/uniprot.sh
+++ mega-mapper/scripts/uniprot.sh
@@ -2,9 +2,9 @@
 
 #DOWNLOAD_BASE=ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete
 DOWNLOAD_BASE=/ebi/extserv/projects/ebinocle/data/uniprot/latest
-SWISSPROT=$DOWNLOAD_BASE/uniprot_sprot.xml.gz
-TREMBL=$DOWNLOAD_BASE/uniprot_trembl.xml.gz
+SWISSPROT=$DOWNLOAD_BASE/uniprot_sprot.xml
+TREMBL=$DOWNLOAD_BASE/uniprot_trembl.xml
 
 XSL=$(dirname $0)/../src/main/resources/uniprot2species-tab.xsl
 
-zcat $SWISSPROT | xsltproc $XSL - > uniprot2species.tab
+xsltproc $XSL $SWISSPROT > SwissProt2species.tab
