fc339903f932bdc4b990d08dfb328f12f4ceb891
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-20 15:48:28 +0000

    Parsing UniProt XML into a lucene index.
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@355 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ src/main/java/uk/ac/ebi/ep/mm/MmField.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.ep.mm;
+
+public enum MmField {
+
+	UNIPROT_ACCESSION,
+	/**
+	 * UniProt entry name (formerly 'UniProt ID').
+	 */
+	UNIPROT_NAME,
+	/**
+	 * Organism scientific name.
+	 */
+	SPECIES,
+	/**
+	 * EC number(s).
+	 */
+	EC
+}
