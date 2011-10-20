e2383a68bf68d7f71f71702bb5e8e854bf3764f3
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-20 15:53:25 +0000

    script using java
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@356 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/uniprot.sh
+++ enzyme-portal/mega-mapper/scripts/uniprot.sh
@@ -5,6 +5,20 @@ DOWNLOAD_BASE=/ebi/extserv/projects/ebinocle/data/uniprot/latest
 SWISSPROT=$DOWNLOAD_BASE/uniprot_sprot.xml
 TREMBL=$DOWNLOAD_BASE/uniprot_trembl.xml
 
-XSL=$(dirname $0)/../src/main/resources/uniprot2species-tab.xsl
+MM_INDEX=/nfs/seqdb/production/intenz/users/rafalcan/tmp/ep/mmIndex
 
-xsltproc $XSL $SWISSPROT > SwissProt2species.tab
+# xsltproc builds the tree in DOM, too much menory.
+#XSL=$(dirname $0)/../src/main/resources/uniprot2species-tab.xsl
+#xsltproc $XSL $SWISSPROT > SwissProt2species.tab
+
+mvn clean package
+
+CP=.
+for JAR in $(dirname $0)/../target/*.jar
+do
+    CP=$CP:$JAR
+done
+
+java -classpath $CP uk.ac.ebi.ep.mm.UniprotSaxParser \
+    -xmlFile $SWISSPROT -indexDir $MM_INDEX
+
