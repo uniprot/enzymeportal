7ea2b410e90e513a3ff44bdec642645c7eaa9a43
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-10-21 15:09:58 +0000

    new scripts
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@365 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- scripts/up-swissprot.sh
+++ scripts/up-swissprot.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+# Creates an initial mega-mapper lucene index with the EB-Eye Swiss-Prot file.
+# Param:
+# $1: directory for the created lucene index.
 
 #DOWNLOAD_BASE=ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete
-DOWNLOAD_BASE=/ebi/extserv/projects/ebinocle/data/uniprot/latest
-SWISSPROT=$DOWNLOAD_BASE/uniprot_sprot.xml
-
-MM_INDEX=/nfs/seqdb/production/intenz/users/rafalcan/tmp/ep/mmIndex
-
-# xsltproc builds the tree in DOM, too much menory.
-#XSL=$(dirname $0)/../src/main/resources/uniprot2species-tab.xsl
-#xsltproc $XSL $SWISSPROT > SwissProt2species.tab
+EBINOCLE_DATA=/ebi/extserv/projects/ebinocle/data
+SWISSPROT=$EBINOCLE_DATA/uniprot/latest/uniprot_sprot.xml
 
 cd $(dirname $0)/..
 mvn clean package
@@ -20,5 +17,5 @@ do
 done
 
 java -classpath $CP uk.ac.ebi.ep.mm.UniprotIndexer \
-    -xmlFile $SWISSPROT -indexDir $MM_INDEX
+    -xmlFile $SWISSPROT -indexDir $1
 
