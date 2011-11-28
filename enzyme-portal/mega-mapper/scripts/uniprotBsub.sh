a9da7a211f3f04646b23a0cba4ba9b82b0375537
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-11-28 11:33:53 +0000

    Outputting times to stdout
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@386 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/uniprotBsub.sh
+++ enzyme-portal/mega-mapper/scripts/uniprotBsub.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 \
-    $(dirname $0)/uniprot.sh
+bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/uniprot.sh
