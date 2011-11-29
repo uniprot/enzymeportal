40d46cb195aa7b42c943ba3a926d9ecdaa55c120
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-11-29 09:32:43 +0000

    more hibernate testing
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@392 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- mega-mapper/scripts/ebeye.sh
+++ mega-mapper/scripts/ebeye.sh
@@ -2,7 +2,6 @@
 # Imports EB-Eye XML files into an existing mega-map.
 # Params:
 # $1: EB-Eye XML file (ChEBI/ChEMBL-target)
-# $2: directory containing an existing mega-mapper lucene index
 
 cd $(dirname $0)/..
 mvn clean package
