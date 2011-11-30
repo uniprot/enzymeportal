b7f040960025fb07d101fadbb28bde1fb0dfc1f0
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-11-30 11:01:21 +0000

    Added classes for filters.
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@395 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/SpeciesDefaultWrapper.java
@@ -0,0 +1,56 @@
+package uk.ac.ebi.ep.core;
+
+import uk.ac.ebi.ep.search.model.Species;
+
+/**
+ * Wrapper around a Species object which provides the missing
+ * <code>equals</code> and <code>hashCode</code> methods (the class
+ * <code>Species</code> is auto-generated from a XML schema and does not
+ * provide them).
+ * <br>
+ * It also implements <code>Comparable</code> to provide a 'natural' order for
+ * Species objects.
+ * <br>
+ * All the implemented methods are based exclusively on the species scientific
+ * name.
+ * @author rafa
+ *
+ */
+public class SpeciesDefaultWrapper implements Comparable<SpeciesDefaultWrapper> {
+
+	private Species species;
+	
+	public Species getSpecies() {
+		return species;
+	}
+
+	public SpeciesDefaultWrapper(Species species){
+		if (species == null) throw new NullPointerException();
+		this.species = species;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + species.getScientificname().hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		SpeciesDefaultWrapper other = (SpeciesDefaultWrapper) obj;
+		if (!species.getScientificname().equals(other.species.getScientificname()))
+			return false;
+		return true;
+	}
+
+	public int compareTo(SpeciesDefaultWrapper other) {
+		// TODO: put Homo sapiens and others at the top.
+		return species.getScientificname().compareTo(other.species.getScientificname());
+	}
+	
+}
---
+++ enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/filter/DiseasesPredicate.java
@@ -0,0 +1,47 @@
+package uk.ac.ebi.ep.core.filter;
+
+import java.util.Collection;
+
+import org.apache.commons.collections.Predicate;
+
+import uk.ac.ebi.ep.search.model.Compound;
+import uk.ac.ebi.ep.search.model.Disease;
+import uk.ac.ebi.ep.search.model.EnzymeAccession;
+import uk.ac.ebi.ep.search.model.EnzymeSummary;
+
+public class DiseasesPredicate implements Predicate {
+
+	/**
+	 * Collection of unique diseases IDs, which will act as a filter.
+	 */
+	private Collection<String> diseasesFilter;
+	
+	public DiseasesPredicate(Collection<String> diseasesFilter) {
+		this.diseasesFilter = diseasesFilter;
+	}
+
+	/**
+	 * Evaluates whether an enzyme summary contains any of the diseases in the
+	 * filter.
+	 * @param obj an EnzymeSummary object.
+	 * @return <code>true</code> if the enzyme summary contains any of the
+	 * 		diseases in the filter, <code>false</code> otherwise.
+	 */
+	public boolean evaluate(Object obj) {
+		boolean eval = false;
+		if (obj instanceof EnzymeSummary){
+			EnzymeSummary es = (EnzymeSummary) obj;
+			for (EnzymeAccession ea : es.getRelatedspecies()){
+				for (Disease disease : ea.getDiseases()) {
+					String id = disease.getId();
+					if (diseasesFilter.contains(id)){
+						eval = true;
+						break;
+					}
+				}
+			}
+		}
+		return eval;
+	}
+
+}
