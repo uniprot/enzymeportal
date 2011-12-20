d4ccb274d3abc97adf9ed9ce4255dd9a1e7976a0
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-12-20 10:29:15 +0000

    Some debugging lines
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/reactome-adapter@443 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/reactome/ReactomeAdapter.java
+++ src/main/java/uk/ac/ebi/ep/reactome/ReactomeAdapter.java
@@ -12,6 +12,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+
 import uk.ac.ebi.ep.enzyme.model.EnzymeModel;
 import uk.ac.ebi.ep.enzyme.model.EnzymeReaction;
 import uk.ac.ebi.ep.enzyme.model.Pathway;
@@ -29,8 +31,10 @@ import uk.ac.ebi.ep.reactome.ReactomeCallable.GetPathwayCaller;
 public class ReactomeAdapter implements IReactomeAdapter{
 
     //seconds
-    public static final int TIME_OUT = 40;
+    public static final int TIME_OUT = 40; // FIXME take me out of here!
 
+    private final Logger logger = Logger.getLogger(ReactomeAdapter.class);
+    
     public String getReactionDescription(String reactomeAccession)
 	throws ReactomeServiceException{
         String reactionDesc = null;
@@ -64,57 +68,59 @@ public class ReactomeAdapter implements IReactomeAdapter{
 
     }
 
+    /**
+     * {@inheritDoc}
+     * This implementation uses any xrefs from reactions, trusting WRONGLY
+     * they are pointing to Reactome.
+     */
     public EnzymeModel addReactionDescriptions(EnzymeModel enzymeModel)
 	throws ReactomeServiceException {
-        //To be done in EnzymeRetriever
-        //queryRheaWsForReactions(enzymeModel);
-
         List<ReactionPathway> reactionPathways = enzymeModel.getReactionpathway();
         //get the reaction and pathways with rhea ws results set
-        //List<ReactionPathway> reactionPathways = enzymeModel.getReactionpathway();
         for (ReactionPathway reactionPathway: reactionPathways) {
-            //List<Pathway> pathways = reactionPathway.getPathways();
             EnzymeReaction enzymeReaction = reactionPathway.getReaction();
            // for (Pathway pathway: pathways) {
             if (enzymeReaction != null) {
                 //get Reactome reaction id retrieved from Rhea
+            	// FIXME: rhea reactions might come with other xrefs (ex. MACiE)
                 List<Object> rheaReactomeLinks = enzymeReaction.getXrefs();
                 String reactomeUrl = null;
                 if (rheaReactomeLinks.size() > 0) {
                     reactomeUrl = (String)rheaReactomeLinks.get(0);
                     try {
-                        //Object[] results = this.reactomeAdapter.getReactionPathway(reactomeUrl);
                         String reactomeAccession = reactomeUrl.split("=")[1];
                         String reactionDesc = getReactionDescription(reactomeAccession);
-
                         enzymeReaction.setDescription(reactionDesc);
                     }
                     catch (ReactomeServiceException ex) {
                         throw new ReactomeServiceException("Failed to retrieve reaction desciption for " +reactomeUrl, ex);
                     }
-
                 }
             }
         }
         return enzymeModel;
     }
 
-    public List<Pathway> getPathways (List<String> stableIds)
+    public List<Pathway> getPathways(List<String> stableIds)
 	throws ReactomeServiceException {
         ExecutorService pool = Executors.newCachedThreadPool();
 	    CompletionService<Pathway> ecs =
 	    		new ExecutorCompletionService<Pathway>(pool);
         List<Pathway> pathways = new ArrayList<Pathway>();
         try {
+        	logger.debug(" -RP- before sending jobs");
 	        for (String id: stableIds) {
 	            GetPathwayCaller callable = new ReactomeCallable.GetPathwayCaller(id);
 	            ecs.submit(callable);
 	        }
+        	logger.debug(" -RP- before retrieving jobs");
 	        for (int i = 0; i < stableIds.size(); i++) {
 				try {
 					Future<Pathway> future = ecs.poll(TIME_OUT, TimeUnit.SECONDS);
 	    			if (future != null){
 		                pathways.add(future.get());
+	    			} else {
+	    				logger.warn("Job did not return");
 	    			}
 	            } catch (InterruptedException ex) {
 	                throw new ReactomeServiceException("The thread was interrupted while retieving from Reactome pathway " +stableIds.get(i), ex);
@@ -125,6 +131,7 @@ public class ReactomeAdapter implements IReactomeAdapter{
         } finally {
             pool.shutdown();
         }
+    	logger.debug(" -RP- before returning");
         return pathways;
     }
 
