12106c78b4401b42f8bb75e479b010f00126d64f
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2011-12-20 10:29:15 +0000

    Some debugging lines
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@443 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/reactome-adapter/src/main/java/uk/ac/ebi/ep/reactome/IReactomeAdapter.java
+++ enzyme-portal/reactome-adapter/src/main/java/uk/ac/ebi/ep/reactome/IReactomeAdapter.java
@@ -20,6 +20,12 @@ public interface IReactomeAdapter {
     public String getPathwayDescription(String reactomeAccession)
     throws ReactomeServiceException;
 
+    /** Gets descriptions for any reactions catalised by the enzyme
+     * and adds them to the model.
+     * @param enzymeModel an enzyme model.
+     * @return the same model updated with reaction descriptions.
+     * @throws ReactomeServiceException
+     */
     public EnzymeModel addReactionDescriptions(EnzymeModel enzymeModel)
     throws ReactomeServiceException;
 
