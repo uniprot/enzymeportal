c190f859f2c54ebd8a50f34191879cf35b3eaa8b
Joseph Onwubiko <joseph@ebi.ac.uk>
2012-01-03 15:36:04 +0000

    Specie ordering : model organisms first, then the rest alphabetically. (i would recommend for the species to have a sorting criteria to minimise the intensive computation required in this specie ordering implementation).
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@457 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/SpeciesDefaultWrapper.java
+++ enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/SpeciesDefaultWrapper.java
@@ -18,39 +18,62 @@ import uk.ac.ebi.ep.search.model.Species;
  */
 public class SpeciesDefaultWrapper implements Comparable<SpeciesDefaultWrapper> {
 
-	private Species species;
-	
-	public Species getSpecies() {
-		return species;
-	}
+    private Species species;
 
-	public SpeciesDefaultWrapper(Species species){
-		if (species == null) throw new NullPointerException();
-		this.species = species;
-	}
+    public Species getSpecies() {
+        return species;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + species.getScientificname().hashCode();
-		return result;
-	}
+    public SpeciesDefaultWrapper(Species species) {
+        if (species == null) {
+            throw new NullPointerException();
+        }
+        this.species = species;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (getClass() != obj.getClass()) return false;
-		SpeciesDefaultWrapper other = (SpeciesDefaultWrapper) obj;
-		if (!species.getScientificname().equals(other.species.getScientificname()))
-			return false;
-		return true;
-	}
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + species.getScientificname().hashCode();
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj) return true;
+//		if (obj == null) return false;
+//		if (getClass() != obj.getClass()) return false;
+//		SpeciesDefaultWrapper other = (SpeciesDefaultWrapper) obj;
+//		if (!species.getScientificname().equals(other.species.getScientificname()))
+//			return false;
+//		return true;
+//	}
+    public int compareTo(SpeciesDefaultWrapper other) {
+        // TODO: put Homo sapiens and others at the top.
+        //return species.getScientificname().compareTo(other.species.getScientificname());
+        return species.getCommonname().compareTo(other.species.getCommonname());
+    }
 
-	public int compareTo(SpeciesDefaultWrapper other) {
-		// TODO: put Homo sapiens and others at the top.
-		return species.getScientificname().compareTo(other.species.getScientificname());
-	}
-	
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SpeciesDefaultWrapper other = (SpeciesDefaultWrapper) obj;
+        if (this.species != other.species && (this.species == null || !this.species.equals(other.species))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + (this.species != null ? this.species.hashCode() : 0);
+        return hash;
+    }
 }
