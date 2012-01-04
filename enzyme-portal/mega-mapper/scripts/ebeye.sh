c0ce2c37dbd533d16d96312c528f6f2f2b293dac
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-01-04 10:35:38 +0000

    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@461 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/ebeye.sh
+++ enzyme-portal/mega-mapper/scripts/ebeye.sh
@@ -8,6 +8,6 @@ EBINOCLE_DATA=/ebi/extserv/projects/ebinocle/data
 . $(dirname $0)/mvnBuild.sh
 
 echo "Starting EB-Eye import - $(date)"
-java -Xms512M -Xmx1G -classpath $CP uk.ac.ebi.ep.mm.EbeyeSaxParser \
-	-xmlFile $1
+java -Xms512M -Xmx1G -classpath $CP \
+	uk.ac.ebi.ep.mm.EbeyeSaxParser -xmlFile $1
 echo "Finished EB-Eye import - $(date)"
