fffc8cbe67c4f8344365c6a0e5307ad7c30d3bb2
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-01-04 10:35:38 +0000

    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@461 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- scripts/up-swissprot.sh
+++ scripts/up-swissprot.sh
@@ -7,9 +7,11 @@
 EBINOCLE_DATA=/ebi/extserv/projects/ebinocle/data
 XML_FILE=${1:-$EBINOCLE_DATA/uniprot/latest/uniprot_sprot.xml}
 
+HIBERNATE_OPTS="-Dhibernate.hbm2ddl.auto=create"
+
 . $(dirname $0)/mvnBuild.sh
 
 echo "Starting Swiss-Prot import - $(date)"
-java -Xms512M -Xmx1G -classpath $CP -Dhibernate.hbm2ddl.auto=create \
+java -Xms512M -Xmx1G -classpath $CP $HIBERNATE_OPTS \
 	uk.ac.ebi.ep.mm.UniprotSaxParser -xmlFile $XML_FILE
 echo "Finished Swiss-Prot import - $(date)"
