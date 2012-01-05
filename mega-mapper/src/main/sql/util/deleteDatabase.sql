396bcf748f7c7ab5874a88752c2c822ea6aea21f
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-01-05 16:45:23 +0000

    no accessions table anymore
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@473 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- mega-mapper/src/main/sql/util/deleteDatabase.sql
+++ mega-mapper/src/main/sql/util/deleteDatabase.sql
@@ -1,10 +1,8 @@
 /*
- * Deletes all accessions, cross references and entries for a given database
+ * Deletes all cross references and entries for a given database
  * from the mega-map.
  * Parameter: &1 - the database name.
  */
-delete from mm_accession ma
-  where exists (select 1 from mm_entry where db_name = '&1' and id = ma.id);
 delete from mm_xref x
   where exists (select 1 from mm_entry where db_name = '&1' and (id = x.from_entry or id = x.to_entry));
 delete from mm_entry where db_name = '&1';
