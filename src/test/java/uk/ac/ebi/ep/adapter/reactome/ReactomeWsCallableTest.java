f215779d32f271d7e104823bb5ae8a83a9e1e1fa
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-01-25 14:15:51 +0000

    Reaction and pathways descriptions are taken now from the Reactome REST web service
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/reactome-adapter@546 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ src/test/java/uk/ac/ebi/ep/adapter/reactome/ReactomeWsCallableTest.java
@@ -0,0 +1,36 @@
+package uk.ac.ebi.ep.adapter.reactome;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.ebi.ep.adapter.reactome.ReactomeWsCallable.ReactomeClass;
+
+public class ReactomeWsCallableTest {
+
+	private ReactomeWsCallable rwc;
+	
+	@Before
+	public void setUp() throws Exception {
+		ReactomeConfig config = new ReactomeConfig();
+		config.setUseProxy(true);
+		config.setWsBaseUrl(ReactomeWsCallable.WS_BASE_URL);
+		rwc = new ReactomeWsCallable(config);
+	}
+
+	@Test
+	public void testGetDescription() throws ReactomeServiceException {
+		String desc;
+		desc = rwc.getDescription(ReactomeClass.Reaction, "REACT_21342");
+		System.out.println(desc);
+		assertNotNull(desc);
+		desc = rwc.getDescription(ReactomeClass.Reaction, "REACT_75928");
+		System.out.println(desc);
+		assertNotNull(desc);
+		desc = rwc.getDescription(ReactomeClass.Pathway, "REACT_15295");
+		System.out.println(desc);
+		assertNotNull(desc);
+	}
+
+}
