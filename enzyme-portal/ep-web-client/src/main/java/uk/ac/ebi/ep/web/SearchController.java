73f593c4137c777346ca325182aeda6ead4c8deb
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-01-31 14:57:11 +0000

    Set configuration before using it :-)
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@564 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/ep-web-client/src/main/java/uk/ac/ebi/ep/web/SearchController.java
+++ enzyme-portal/ep-web-client/src/main/java/uk/ac/ebi/ep/web/SearchController.java
@@ -96,8 +96,8 @@ public class SearchController {
                     enzymeModel = retriever.getProteinStructure(accession);
                     break;
                 case reactionsPathways:                 
-                    enzymeModel = retriever.getReactionsPathways(accession);
                     retriever.getReactomeAdapter().setConfig(reactomeConfig);
+                    enzymeModel = retriever.getReactionsPathways(accession);
                     break;
                 case molecules:
                     enzymeModel = retriever.getMolecules(accession);
