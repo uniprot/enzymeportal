21d000d8dd9197982dca5ef596688bee420456d4
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-02-06 11:06:50 +0000

    Cleaned scripts
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@601 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- mega-mapper/scripts/mvnBuild.sh
+++ mega-mapper/scripts/mvnBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Build java code and prepare environment variables to run the mega-mapper.
 
 cd $(dirname $0)/..
 mvn -DskipTests=true -P apps,dev clean package
@@ -8,3 +9,5 @@ for JAR in target/*.jar
 do
     CP=$CP:$JAR
 done
+
+JAVA_OPTS='-Xms512M -Xmx1G'
