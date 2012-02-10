662aff4aaf2cd5290065d5e18c18b3dd9dda12f4
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-02-10 08:44:29 +0000

    script to send the job to LSF
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@637 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ scripts/mmBsub.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Creates a complete mega-map, replacing the existing one. It asks for a
+# password to delete all data and then sends the script mm.sh to LSF.
+# Param:
+# $1: database environment (enzdev|ezprel)
+
+echo
+echo "**************************** W A R N I N G ****************************"
+echo "THIS WILL OVERWRITE ALL DATA IN $1. ARE YOU SURE?"
+echo "(Ctrl-C to cancel, Enter to continue)"
+read ok
+
+read -p "Password for enzyme_portal@${1}:" -s PASSWORD
+
+cd $(dirname $0)/../src/main/sql
+
+echo
+echo "*** Dropping everything in $1..."
+echo sqlplus $1 @drop_all
+echo exit | sqlplus enzyme_portal/${PASSWORD}@${1} @delete_all
+
+bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/mm.sh
