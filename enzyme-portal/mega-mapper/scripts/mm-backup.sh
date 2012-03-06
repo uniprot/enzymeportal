c64e75a331cd55c7a0a99bb85f181dd2811743bc
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-03-06 15:37:29 +0000

    added exp parameters file
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@736 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/mm-backup.sh
+++ enzyme-portal/mega-mapper/scripts/mm-backup.sh
@@ -9,6 +9,6 @@ EP_BACKUP_DIR=/nfs/panda/production/steinbeck/ep/backup
 DB_DUMP=$EP_BACKUP_DIR/ep-mm-$(date +%Y%m%d_%H%M).dmp
 
 $ORACLE_HOME/bin/exp ${DB_USER}/${DB_PASSWD}@${1} \
-    parfile=$(dirname $0)/export.par file=$DB_DUMP
+    parfile=$(dirname $0)/ora-exp.par file=$DB_DUMP
 gzip $DB_DUMP
 
