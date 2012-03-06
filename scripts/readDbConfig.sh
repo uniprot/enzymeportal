8551707ff7fad0c589b8448a418d6cc6ee68d520
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-03-06 15:37:08 +0000

    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@735 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- scripts/readDbConfig.sh
+++ scripts/readDbConfig.sh
@@ -3,12 +3,11 @@
 # Param:
 # $1: database environment (enzdev|ezprel)
 
-EP_CONFIG_DIR=/nfs/panda/production/steinbeck/ep/config
-
 . $(dirname $0)/checkParams.sh
 
-DB_CONFIG=ep-mm-db-${1}.properties
-DB_USER=$(grep '^user=' $EP_CONFIG_DIR/$DB_CONFIG | cut -d '=' -f 2)
-DB_PASSWD=$(grep '^password=' $EP_CONFIG_DIR/$DB_CONFIG | cut -d '=' -f 2)
-#DB_INSTANCE=$(grep '^instance=' $EP_CONFIG_DIR/$DB_CONFIG | cut -d '=' -f 2)
+EP_CONFIG_DIR=/nfs/panda/production/steinbeck/ep/config
+DB_CONFIG=$EP_CONFIG_DIR/ep-mm-db-${1}.properties
+DB_USER=$(grep '^user=' $DB_CONFIG | cut -d '=' -f 2)
+DB_PASSWD=$(grep '^password=' $DB_CONFIG | cut -d '=' -f 2)
+#DB_INSTANCE=$(grep '^instance=' $DB_CONFIG | cut -d '=' -f 2)
 
