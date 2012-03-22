cac44aaa056a27667845c34e8b73b00a16bfb006
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-03-22 16:26:40 +0000

    adding new statistics files to svn
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/mega-mapper@766 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- scripts/mm-statistics.sh
+++ scripts/mm-statistics.sh
@@ -7,9 +7,17 @@
 . $(dirname $0)/ts.sh
 
 STATS_SQL=$(dirname $0)/../src/main/sql/util/statistics
-TSV_FILE=$(dirname $0)/../src/site/resources/statistics/stats-${NOW}.tsv
+STATS_DIR=$(dirname $0)/../src/site/resources/statistics
+TSV_FILE=${STATS_DIR}/stats-${NOW}.tsv
 
 echo "Generating statistics..."
 sqlplus ${DB_USER}/${DB_PASSWD}@${1} @${STATS_SQL} ${TSV_FILE}
 echo "Statistics dumped to file $TSV_FILE"
+
+# Add the statistics file to svn and commit:
+while read SVN_ADD
+do
+    svn add ${SVN_ADD:8}
+    svn ci -m 'new release' ${SVN_ADD:8}
+done < <(svn st $STATS_DIR | grep '^\?')
 
