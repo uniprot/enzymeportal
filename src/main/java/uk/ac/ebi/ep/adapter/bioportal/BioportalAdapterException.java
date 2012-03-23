f1c2d7435760248c8299317261055665ad0091c8
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-03-23 12:54:19 +0000

    Created site directory and moved xdoc and site.xml there. Added documentation.
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/bioportal-adapter@767 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/adapter/bioportal/BioportalAdapterException.java
+++ src/main/java/uk/ac/ebi/ep/adapter/bioportal/BioportalAdapterException.java
@@ -1,5 +1,10 @@
 package uk.ac.ebi.ep.adapter.bioportal;
 
+/**
+ * Exception thrown while querying BioPortal.
+ * @author rafa
+ *
+ */
 public class BioportalAdapterException extends Exception {
 
 	private static final long serialVersionUID = -7780365580065755918L;
