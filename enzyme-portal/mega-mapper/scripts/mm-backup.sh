51c8fedafc4a2e6e4c71b739ce3e160aa3d745c5
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-03-26 17:01:33 +0000

    fixed script (it is a variable, not a command)
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@776 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/mm-backup.sh
+++ enzyme-portal/mega-mapper/scripts/mm-backup.sh
@@ -7,7 +7,7 @@
 . $(dirname $0)/ts.sh
 
 EP_BACKUP_DIR=/nfs/panda/production/steinbeck/ep/backup
-DB_DUMP=$EP_BACKUP_DIR/ep-mm-$(NOW).dmp
+DB_DUMP=$EP_BACKUP_DIR/ep-mm-${NOW}.dmp
 
 echo "Creating backup..."
 $ORACLE_HOME/bin/exp ${DB_USER}/${DB_PASSWD}@${1} \
