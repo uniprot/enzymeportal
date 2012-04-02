761723f49ba31c7b4b33241e496698e995361a6c
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-04-02 13:44:38 +0000

    new script to load from backup
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@794 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ enzyme-portal/mega-mapper/scripts/loadFromBackup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Refresh data from a backup of the mega-map.
+# Param:
+# $1: database instance to refresh (enzdev|ezprel)
+# $2: backup file (may be gzipped)
+
+echo
+echo "**************************** W A R N I N G ****************************"
+echo "THIS WILL OVERWRITE ALL DATA IN $1. ARE YOU SURE?"
+echo "(Ctrl-C to cancel, Enter to continue)"
+read ok
+
+. $(dirname $0)/readDbConfig.sh $1
+
+[ ! -e "$2" ] && echo "Please especify an oracle dump file" && exit 1
+
+# Check if dump file is gzipped, unpack if needed:
+if [ "$(file $1 | grep -c 'gzip compressed data')" == 1 ]
+then
+    echo "$1 - dump file is gzipped"
+    # Unpack dump file
+    TMP_DMP=$$.dmp
+    gunzip -c $1 > $TMP_DMP
+    DMP=$TMP_DMP
+fi
+
+. $(dirname $0)/mm-delete.sh $1
+
+echo "Importing from backup..."
+$ORACLE_HOME/bin/imp ${DB_USER}/${DB_PASSWD}@enzdev \
+    file=$DMP log=$(dirname $0)/ep-mm-imp.log
+
+# Clean up:
+[ -n "$TMP_DMP" ] && rm $TMP_DMP
+
