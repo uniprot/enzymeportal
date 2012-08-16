e5fde39625a665f145aed571f02f8ccb7ebc24c5
Joseph Onwubiko <joseph@ebi.ac.uk>
2012-08-16 15:32:10 +0000

    updating ep sitemapGenerator
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal/SitemapGenerator@1077 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- src/main/java/uk/ac/ebi/ep/sitemap/advanced/SitemapGenerator.java
+++ src/main/java/uk/ac/ebi/ep/sitemap/advanced/SitemapGenerator.java
@@ -63,7 +63,7 @@ abstract class SitemapGenerator<U extends ISitemapUrl, THIS extends SitemapGener
         if (finished) {
             throw new RuntimeException("Sitemap already printed; you must create a new generator to make more sitemaps");
         }
-        UrlUtils.checkUrl(url.getUrl().toString(), baseUrl);
+       // UrlUtils.checkUrl(url.getUrl().toString(), baseUrl);
         if (urls.size() == maxUrls) {
             if (!allowMultipleSitemaps) {
                 throw new RuntimeException("More than " + maxUrls + " urls, but allowMultipleSitemaps is false.  Enable allowMultipleSitemaps to split the sitemap into multiple files with a sitemap index.");
