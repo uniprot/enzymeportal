df2713e85be82c436e13cbaf443b3fda4087753a
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-08-17 12:45:00 +0000

    No System.out.println, which clutters catalina.out
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk/enzyme-portal@1081 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- ep-util/src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
+++ ep-util/src/main/java/uk/ac/ebi/ep/util/query/LuceneQueryBuilder.java
@@ -3,9 +3,10 @@ package uk.ac.ebi.ep.util.query;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+
 import uk.ac.ebi.biobabel.lucene.LuceneParser;
-//import uk.ac.ebi.ebeye.ParamOfGetResultsIds;
-//import uk.ac.ebi.ebeye.ResultOfGetNumberOfResults;
 import uk.ac.ebi.ep.config.Domain;
 import uk.ac.ebi.ep.config.SearchField;
 import uk.ac.ebi.ep.search.model.SearchParams;
@@ -28,6 +29,9 @@ public class LuceneQueryBuilder {
     public static final String ENZYME_FILTER_UNIPROTAPI ="ec:(1.* OR 2.* OR 3.* OR 4.* OR 5.* OR 6.*)";
     public static final String UNIPROT_SPECIES_FIELD ="organism";
     public static final String EBEYE_SPECIES_FIELD ="organism_scientific_name";
+
+    public static final Logger LOGGER =
+            Logger.getLogger(LuceneQueryBuilder.class);
 
     //133343 results does not pass the load test
     //public static final int MAX_RESULTS = 100000;
@@ -298,7 +302,7 @@ public class LuceneQueryBuilder {
         //String filterQuery = LuceneQueryBuilder.a.createQueryIN(filterFieldName, false, filterValues);
         for (String query:queries) {
             String qryWithSpecies = addSpeciesFilterQuery(query, filterFieldName, filterValues);
-            System.out.println(qryWithSpecies);
+            LOGGER.debug(qryWithSpecies);
             queriesWithFilter.add(qryWithSpecies);
         }
         return queriesWithFilter;
