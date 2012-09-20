cd50bf685a9640edf7c202519f3c124efb3f375e
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2012-09-20 13:48:24 +0000

    copying new statistics generated with a tag to the trunk
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@1180 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

--- enzyme-portal/mega-mapper/scripts/mm-statistics.sh
+++ enzyme-portal/mega-mapper/scripts/mm-statistics.sh
@@ -15,9 +15,18 @@ sqlplus ${DB_USER}/${DB_PASSWD}@${1} @${STATS_SQL} ${TSV_FILE}
 echo "Statistics dumped to file $TSV_FILE"
 
 # Add the statistics file to svn and commit:
+TRUNK_URL=$(svn info | grep ^URL | sed 's/^URL: //' | \
+    sed 's/trunk\/.*//;s/tags\/.*//;s/branches\/.*//')trunk/enzyme-portal/mega-mapper
+# Are we working with the trunk (0) or not (1)?:
+IS_TRUNK=$(svn info | grep $TRUNK_URL)
+
 while read SVN_ADD
 do
     svn add ${SVN_ADD:8}
     svn ci -m 'new release' ${SVN_ADD:8}
+    if [ ! "$IS_TRUNK" ]
+    then
+        svn cp ${SVN_ADD:8} $TRUNK_URL/${SVN_ADD:8} -m 'copying to trunk'
+    fi
 done < <(svn st $STATS_DIR | grep '^\?')
 
