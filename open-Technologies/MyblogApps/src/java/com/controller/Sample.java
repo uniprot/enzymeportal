1c6383b6773afeb9cb4aacb3c9ca2b0f88bd8a53
Joseph Onwubiko <joseph@ebi.ac.uk>
2012-11-05 13:51:01 +0000

    sample commit
    git-svn-id: https://enzymeportal.svn.sourceforge.net/svnroot/enzymeportal/trunk@1310 e52ccfb7-9281-4bd8-84e1-ceba62799ddf

---
+++ open-Technologies/MyblogApps/src/java/com/controller/Sample.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+/**
+ *
+ * @author joseph
+ */
+public class Sample {
+
+    public Sample() {
+    }
+    
+}
