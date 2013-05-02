b9de2f6121f9044725cf583376d979c645638e15
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2013-05-02 16:40:33 +0100

    New regexp for inhibitors, with tests.

--- enzyme-portal/uniprot-adapter/src/test/java/uk/ac/ebi/ep/adapter/uniprot/TransformerTest.java
+++ enzyme-portal/uniprot-adapter/src/test/java/uk/ac/ebi/ep/adapter/uniprot/TransformerTest.java
@@ -5,13 +5,12 @@
 
 package uk.ac.ebi.ep.adapter.uniprot;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.util.List;
 
-import uk.ac.ebi.ep.adapter.uniprot.Transformer;
+import org.junit.*;
+import uk.ac.ebi.ep.enzyme.model.Molecule;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  *
@@ -38,29 +37,264 @@ public class TransformerTest {
     public void tearDown() {
     }
 
-
-    /**
-     * Test of parseTextForMolecules method, of class Transformer.
-     */
     @Test
-    public void testParseTextForMolecules() {
-        System.out.println("parseTextForMolecules");
-        //String text = "Completely inhibited by ADP and ADP-glucose, and partially inhibited by ATP and NADH";
-        String text = "Activated by threonine and tyrosine phosphorylation by either of two dual specificity kinases, MAP2K3 or MAP2K6, and potentially also MAP2K4. Inhibited by dual specificity phosphatases, such as DUSP1. Specifically inhibited by the binding of pyridinyl-imidazole compounds, which are cytokine-suppressive anti-inflammatory drugs (CSAID). Isoform Mxi2 is 100-fold less sensitive to these agents than the other isoforms and is not inhibited by DUSP1";
-        //Transformer.parseTextForActivators(text);
-        String[] sentences = text.split("\\.");
-        for (String sentence: sentences) {
-            if (sentence.contains("Activated by") || sentence.contains("activated by")) {
-                Transformer.parseTextForActivators(sentence.trim());
-            }
-            if (sentence.contains("Inhibited by") || sentence.contains("inhibited by")) {
-                Transformer.parseTextForInhibitors(sentence.trim());
-            }
+    public void testParseTextForInhibitors() {
+        String text;
+        List<Molecule> inhibitors;
+        String[] names;
+
+        text = "Inhibited by sucrose";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("sucrose", inhibitors.get(0).getName());
+
+        text = "Not inhibited by sucrose.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(0, inhibitors.size());
+
+        text = "Inhibited by a and b.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(2, inhibitors.size());
+        assertEquals("a", inhibitors.get(0).getName());
+        assertEquals("b", inhibitors.get(1).getName());
+
+        text = "Inhibited by c, b, a. Activated by m, n. Does this and that." +
+                " Inactivated by foo.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "a", "b", "c", "foo" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
         }
 
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-        
+        text = "Inactivated by bar or Baz.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(2, inhibitors.size());
+        assertEquals("bar", inhibitors.get(0).getName());
+        assertEquals("Baz", inhibitors.get(1).getName());
+
+        text = "Activated by something.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(0, inhibitors.size());
+
+        text = "Inhibited by z; inactivated by y and x.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "x", "y", "z" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Inhibited by EDTA, zinc, silver, copper, and activated by" +
+                " magnesium and calcium. A blah blah...";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "copper","EDTA","silver","zinc" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Is inhibited by H2O2. 1,10-phenanthroline inhibits the" +
+                " activity slightly, but ...";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("H2O2", inhibitors.get(0).getName());
+
+        text = "Inhibited by manganese, copper, mercury, and iron ions.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "copper","iron ions","manganese","mercury" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Completely inhibited by 5 mM N-ethylmaleimide or 0.1 mM Cu2+." +
+                " Partially inhibited by 0.1 uM Fe2+ or 2 mM Hg2+.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "Cu2+", "Fe2+", "Hg2+", "N-ethylmaleimide"};
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Completely inhibited by CuCl2, FeCl3, HgCl2 and" +
+                " N-bromosuccinimide. Moderately inhibited by AgCl, AlCl3," +
+                " Pb(CH3COO)2 and dithiothreitol. BaCl2, CaCl2, KCl, MgCl2," +
+                " MnCl2, NaCl, ZnCl2, ethylenediaminetetraacetic acid," +
+                " N-ethylmaleimide, iodoacetic acid and" +
+                " p-chloromercuribenzoic acid have little or no effect on" +
+                " activity.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "AgCl", "AlCl3", "CuCl2", "dithiothreitol",
+                "FeCl3", "HgCl2", "N-bromosuccinimide", "Pb(CH3COO)2" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Completely inhibited by 10 mM p-coumaric acid, this" +
+                " inhibition is rapid, reversible and non-competitive." +
+                " Completely inhibited by 0.1 mM Cu2+, 0.1 mM Hg2+ and 10 mM" +
+                " caffeic acid. Partially inhibited by 5 mM N-ethylmaleimide," +
+                " 1 mM diethylpyrocarbonate and 1 mM acetyl-CoA.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "acetyl-CoA", "caffeic acid", "Cu2+",
+                "diethylpyrocarbonate", "Hg2+", "N-ethylmaleimide",
+                "p-coumaric acid"};
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Inhibited by high levels of AMP.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("AMP", inhibitors.get(0).getName());
+
+        text = "Inhibited competitively by nicotinic acid with a Ki of" +
+                " 0.49 mM. Inhibited by thiol-specific compounds" +
+                " p-chloromercuribenzoate, DTNB, Ag2SO4, HgCl2, CuCl2 and" +
+                " N-ethylmaleimide. No inhibition by o-phenanthroline," +
+                " 8-hydroxyquinoline, EDTA, disodium" +
+                " 4,5-dihydroxy-m-benzenedisulfonate, fluoride, azide, KCl," +
+                " LiCl, NaCl, BaCl2, MnCl2, MgCl2, PBCl, ZnCl2, CoCl2, SnCl2," +
+                " FeSO4, FeCl3, NiCl2, CdCl2, AlCl3, iodoacetic acid," +
+                " hydro-xylamine, phenylhydrazine, semicarbazide, cysteamine," +
+                " alpha,alpha-dipyridyl and urea.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "Ag2SO4", "CuCl2", "DTNB", "HgCl2",
+                "N-ethylmaleimide", "nicotinic acid",
+                "p-chloromercuribenzoate" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "2-hydroxy-dATPase activity is inhibited by 2-OH-dADP," +
+                " 8-OH-dGDP and 8-OH-dGTP. 8-OH-dGTPase activity is" +
+                " inhibited by 8-OH-dGDP, 2-OH-dADP and 2-OH-dATP";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "2-OH-dADP", "2-OH-dADP", "2-OH-dATP",
+                "8-OH-dGDP", "8-OH-dGDP", "8-OH-dGTP" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Inhibited with low affinity by edelfosine.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("edelfosine", inhibitors.get(0).getName());
+
+        text = "Inhibited by captopril and, to a lesser extent, by" +
+                " lisinopril, trandolaprilat, fosinoprilat and enalaprilat.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "captopril", "enalaprilat", "fosinoprilat",
+                "lisinopril", "trandolaprilat" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Inhibited by AIM-100" +
+                " (4-amino-5,6-biaryl-furo[2,3-d]pyrimidine), which" +
+                " suppresses activating phosphorylation at Tyr-284." +
+                " Repressed by dasatinib.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("AIM-100 (4-amino-5,6-biaryl-furo[2,3-d]pyrimidine)",
+                inhibitors.get(0).getName());
+
+        text = "Inhibited by metalloproteinase inhibitor 3 (TIMP-3), but" +
+                " not by TIMP-1, TIMP-2 and TIMP-4.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("metalloproteinase inhibitor 3 (TIMP-3)",
+                inhibitors.get(0).getName());
+
+        text = "Inhibited by adenosine deaminase inhibitor 2-DCF, but not" +
+                " by adenosine deaminase 1 inhibitor EHNA.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("adenosine deaminase inhibitor 2-DCF",
+                inhibitors.get(0).getName());
+
+        text = "Activated by manganese or magnesium ions. In the presence of" +
+                " magnesium ions, the enzyme is activated by bicarbonate" +
+                " while in the presence of manganese ions, the enzyme is" +
+                " inhibited by bicarbonate. In the absence of magnesium and" +
+                " bicarbonate, the enzyme is weakly activated by calcium.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("bicarbonate", inhibitors.get(0).getName());
+
+        text = "Inhibited by NaBH4 in the presence of FBP.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(1, inhibitors.size());
+        assertEquals("NaBH4", inhibitors.get(0).getName());
+
+        text = "Inhibited by pyridoxal 5-phosphate, bathophenanthroline," +
+                " mersalyl, p-hydroxymercuribenzoate and tannic acid.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        names = new String[]{ "bathophenanthroline", "mersalyl",
+                "p-hydroxymercuribenzoate", "pyridoxal 5-phosphate",
+                "tannic acid" };
+        assertEquals(names.length, inhibitors.size());
+        for (int i = 0; i < inhibitors.size(); i++){
+            assertEquals(names[i], inhibitors.get(i).getName());
+        }
+
+        text = "Inhibited by acetylation at Lys-642 and activated by" +
+                " deacetylation.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(0, inhibitors.size());
+
+        text = "Activated by calcium/calmodulin. Inhibited by the G protein" +
+                " beta and gamma subunit complex.\n";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(0, inhibitors.size());
+
+        text = "Inhibited by substrate concentrations above 0.5 mM.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        assertEquals(0, inhibitors.size());
+
+        // P14618
+        text = "Isoform M2 is allosterically activated by D-fructose" +
+                " 1,6-bisphosphate (FBP). Inhibited by oxalate and" +
+                " 3,3',5-triiodo-L-thyronine (T3). The activity of the" +
+                " tetrameric form is inhibited by PML. Selective binding to" +
+                " tyrosine-phosphorylated peptides releases the allosteric" +
+                " activator FBP, leading to inhibition of PKM enzymatic" +
+                " activity, this diverts glucose metabolites from energy" +
+                " production to anabolic processes when cells are stimulated" +
+                " by certain growth factors. Glycolytic flux are highly" +
+                " dependent on de novo biosynthesis of serine and glycine," +
+                " and serine is a natural ligand and allosteric activator of" +
+                " isoform M2.";
+        inhibitors = Transformer.parseTextForInhibitors(text);
+        // oxalate, 3,3',5-triiodo-L-thyronine, PML
+        assertEquals(3, inhibitors.size());
     }
 
+    @Test
+    public void testParseTextForActivators() throws Exception {
+        String text;
+        // P14618
+        text = "Isoform M2 is allosterically activated by D-fructose" +
+                " 1,6-bisphosphate (FBP). Inhibited by oxalate and" +
+                " 3,3',5-triiodo-L-thyronine (T3). The activity of the" +
+                " tetrameric form is inhibited by PML. Selective binding to" +
+                " tyrosine-phosphorylated peptides releases the allosteric" +
+                " activator FBP, leading to inhibition of PKM enzymatic" +
+                " activity, this diverts glucose metabolites from energy" +
+                " production to anabolic processes when cells are stimulated" +
+                " by certain growth factors. Glycolytic flux are highly" +
+                " dependent on de novo biosynthesis of serine and glycine," +
+                " and serine is a natural ligand and allosteric activator of" +
+                " isoform M2.";
+        List<Molecule> activators = Transformer.parseTextForActivators(text);
+
+        // D-fructose 1,6-bisphosphate
+        assertEquals(1, activators.size());
+    }
 }
