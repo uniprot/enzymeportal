d6cd05387b13d5065d393993eb37fb6094077d6d
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2013-05-07 16:02:29 +0100

    Better regex for inhibitors/activators, with plenty of tests (real  enzyme regulation comments from UniProt).

--- enzyme-portal/ep-util/src/main/java/uk/ac/ebi/ep/util/EPUtil.java
+++ enzyme-portal/ep-util/src/main/java/uk/ac/ebi/ep/util/EPUtil.java
@@ -1,12 +1,89 @@
 package uk.ac.ebi.ep.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import uk.ac.ebi.ep.enzyme.model.Molecule;
 
 public class EPUtil {
+
+    private static final String REGEXP_HOW =
+            "(?: with low affinity| competitively)?";
+    private static final String REGEXP_PROHIB =
+            "(?!(?:auto|de)?phosphorylation|(?:de)?acetylation|substrate|the " +
+                    "|[\\w\\s-]+?binding)";
+    private static final String REGEXP_REL_CONC =
+            "(?:(?:high|low) levels? of )?";
+    private static final String REGEXP_COMP_GRP =
+            "(?:thiol-specific compounds |reductants such as |.*?antibiotics " +
+                    "|.*?drugs? |.*?ions, such as " +
+                    "|various compounds, including )?";
+//            "(?:.*?(?:compound|reductant|antibiotic|drug|ion)s?" +
+//                    "(?: |,? such as |,? including ))?";
+    private static final String REGEXP_EXPLANATION =
+            "(?:,? an? .+?|,? whose .+?|,? which .+?|,? irrespective .+?" +
+                    "| to .+?| with a .+?| at .+?|, this [^\\.]+" +
+                    "|(?: while)? in .+?| binding)?";
+    private static final String INH_REGEXP_OTHERS_LESS =
+            ",? and(?! activated by)" +
+            "(?:, to a (?:lower|lesser) extent, by| slightly inhibited by| by)?";
+    private static final String ACT_REGEXP_OTHERS_LESS =
+            ",? and(?! in(?:hibited|activated) by)" +
+            "(?:, to a (?:lower|lesser) extent, by| slightly activated by| by)?";
+    private static final String INH_REGEXP_DONT_CONTINUE =
+            "(?! (?:and )?activated by| but not | the )";
+    private static final String ACT_REGEXP_DONT_CONTINUE =
+            "(?! (?:and )?in(?:hibited|activated) by| but not | the )";
+    private static final String INH_REGEXP_END =
+            "(?:\\.?$|[\\.;:] |,? but not " +
+                    "|,?(?: and)?(?: \\w+ly)? activated by)";
+    private static final String ACT_REGEXP_END =
+            "(?:\\.?$|[\\.;:] |,? but not " +
+                    "|,?(?: and)?(?: \\w+ly)? in(?:hibited|activated) by)";
+
+    /**
+     * This regexp has got 2 capturing groups.
+     */
+    private static final String INHIBITOR_REGEXP =
+            "(?<![Nn]ot )\\b[Ii]n(?:hibited|activated)" +
+                    REGEXP_HOW +
+                    " by " +
+                    REGEXP_PROHIB +
+                    REGEXP_REL_CONC +
+                    REGEXP_COMP_GRP +
+                    "(.+?)" +
+                    REGEXP_EXPLANATION +
+                    "(?:" +
+                        "(?: or|"+INH_REGEXP_OTHERS_LESS
+                            + "|,"+INH_REGEXP_DONT_CONTINUE+") " +
+                        "(.+?)" +
+                        REGEXP_EXPLANATION +
+                    ")?" +
+                    INH_REGEXP_END;
+
+    private static final String ACTIVATOR_REGEXP =
+            "(?<![Nn]ot )\\b[Aa]ctivated" +
+                    REGEXP_HOW +
+                    " by " +
+                    REGEXP_PROHIB +
+                    REGEXP_REL_CONC +
+                    REGEXP_COMP_GRP +
+                    "(.+?)" +
+                    REGEXP_EXPLANATION +
+                    "(?:" +
+                        "(?: or|"+ACT_REGEXP_OTHERS_LESS
+                            + "|,"+ACT_REGEXP_DONT_CONTINUE+") " +
+                        "(.+?)" +
+                        REGEXP_EXPLANATION +
+                    ")?" +
+                    ACT_REGEXP_END;
+
+    private static final Pattern INHIBITOR_PATTERN =
+            Pattern.compile(INHIBITOR_REGEXP);
+
+    private static final Pattern ACTIVATOR_PATTERN =
+            Pattern.compile(ACTIVATOR_REGEXP);
 
     /**
      * Extracts the prefix from a UniProt ID (strips the species suffix).
@@ -46,4 +123,62 @@ public class EPUtil {
 		}
 		return new ArrayList<String>(wIds);
 	}
+
+    /**
+     * Parses a text to get a list of molecules acting as inhibitors.
+     * @param text an enzyme regulation comment from UniProt.
+     * @return A list of molecules with just a name (no ID).
+     */
+    public static List<Molecule> parseTextForInhibitors(String text){
+        return getMolecules(INHIBITOR_PATTERN.matcher(text));
+    }
+
+    /**
+     * Parses a text to get a list of molecules acting as activators.
+     * @param text an enzyme regulation comment from UniProt.
+     * @return A list of molecules with just a name (no ID).
+     */
+    public static List<Molecule> parseTextForActivators(String text){
+        return getMolecules(ACTIVATOR_PATTERN.matcher(text));
+    }
+
+    /**
+     * Extracts and cleans names from a matcher, building a molecule for each
+     * of them.<br/>
+     * <i>Cleaning</i> includes removing conjunctions from the text and also
+     * chemical concentrations.
+     * @param m a matcher for activators/inhibitors patterns.
+     * @return a list of molecules with just a name. The list can be empty, but
+     *      never <code>null</code>.
+     */
+    private static List<Molecule> getMolecules(Matcher m) {
+        Set<String> names = new TreeSet<String>(new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s1.compareToIgnoreCase(s2);
+            }
+        });
+        List<Molecule> inhibitors = new ArrayList<Molecule>();
+        while (m.find()){
+            for (int i = 1; i <= m.groupCount(); i++){
+                if (m.group(i) == null) continue;
+                String[] namesArray = m.group(i)
+                        // remove concentrations (ex: 1 mM, 0.5 nM)
+                        .replaceAll("\\d+(\\.\\d+)? .M ", "")
+                        // remove stop words
+                        .replaceAll(",? and|,? or", ",")
+                        .split(", ");
+                names.addAll(Arrays.asList(namesArray));
+            }
+        }
+        for (String name: names){
+            // XXX What to do with synonyms in parentheses?
+            // ex: "AIM-100 (4-amino-5,6-biaryl-furo[2,3-d]pyrimidine)"
+            // BUT: "Fe (2+)"
+            Molecule inhibitor = new Molecule();
+            inhibitor.setName(name);
+            inhibitors.add(inhibitor);
+        }
+        return inhibitors;
+    }
+
 }
