b8c51e41750e2bf48e89448ae176ab8817178cf6
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2013-06-11 13:08:21 +0100

    Deleting all means deleting also the data in the new UNIPROT2COMPOUND table. Added script to grant access to the webapp.

--- enzyme-portal/mega-mapper/src/main/sql/delete_all.sql
+++ enzyme-portal/mega-mapper/src/main/sql/delete_all.sql
@@ -1,3 +1,4 @@
 delete from mm_accession;
 delete from mm_xref;
 delete from mm_entry;
+delete from uniprot2compound;
---
+++ enzyme-portal/mega-mapper/src/main/sql/grant_web.sql
@@ -0,0 +1,4 @@
+grant select to enzyme_webserver on mm_accession;
+grant select to enzyme_webserver on mm_entry;
+grant select to enzyme_webserver on mm_xref;
+grant select to enzyme_webserver on uniprot2compound;
