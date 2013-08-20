820cd41c2ff06185d62e4244b5b203739774c5c2
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2013-08-20 15:23:32 +0100

    Script to provide cross-references to ChEBI. Some doc about it.

--- enzyme-portal/mega-mapper/scripts/mm-chebiXrefs.sh
+++ enzyme-portal/mega-mapper/scripts/mm-chebiXrefs.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Generates a XML file with xrefs for ChEBI.
+# Creates a XML file with cross-references for ChEBI.
 # Param:
-# $1: database environment (enzdev|ezprel)
+# $1: database environment (enzdev|ezprel).
 
 . $(dirname $0)/readDbConfig.sh
-. $(dirname $0)/ts.sh
 
-EP_BACKUP_DIR=/nfs/panda/production/steinbeck/ep/backup
-XMLFILE=$EP_BACKUP_DIR/chebiXrefs-${1}-${NOW}.xml
+CM_RO_DIR=/nfs/public/ro/cm
+CHEBI_EP_XREFS_XML=$CM_RO_DIR/other/ep/chebi-ep-xrefs.xml
 
-echo -e "Generating cross-references XML for ChEBI from $1...\n"
-echo 'exit' | sqlplus -S ${DB_USER}/${DB_PASSWD}@${1} \
-    @$(dirname $0)/../src/main/sql/util/chebiXrefs.sql > $XMLFILE
-echo "XML file generated: $XMLFILE"
+echo "Creating chebi-ep-xrefs.xml file..."
+$ORACLE_HOME/bin/sqlplus ${DB_USER}/${DB_PASSWD}@${1} \
+    @$(dirname $0)/../src/main/sql/util/chebiXrefs.sql $CHEBI_EP_XREFS_XML
+echo "Created"
 
