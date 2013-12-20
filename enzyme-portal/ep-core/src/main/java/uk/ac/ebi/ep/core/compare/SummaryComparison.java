f75c5f64e96f87aa7ab2e876874c8612b2357f61
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2013-12-20 12:17:07 +0100

    Fixed NPE when there is no species common name. Fixed basket tab so that it shows the number of summaries. Fixed markup of documentation.

--- enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/compare/SummaryComparison.java
+++ enzyme-portal/ep-core/src/main/java/uk/ac/ebi/ep/core/compare/SummaryComparison.java
@@ -24,19 +24,26 @@ public class SummaryComparison extends AbstractComparison<EnzymeModel> {
 
     @Override
     protected void getSubComparisons(EnzymeModel e1, EnzymeModel e2) {
-        subComparisons.put("Name",
-                new StringComparison(e1.getName(), e2.getName()));
-        String sp1 = new StringBuilder(e1.getSpecies().getCommonname())
-                .append(" (").append(e1.getSpecies().getScientificname())
-                .append(")").toString();
-        String sp2 = new StringBuilder(e2.getSpecies().getCommonname())
-            .append(" (").append(e2.getSpecies().getScientificname())
-            .append(")").toString();
-        subComparisons.put("Species", new StringComparison(sp1, sp2));
+        subComparisons.put("Name", new StringComparison(
+                e1.getName(), e2.getName()));
+        StringBuilder sb1 =
+                new StringBuilder(e1.getSpecies().getScientificname());
+        if (e1.getSpecies().getCommonname() != null){
+            sb1.append(")").insert(0, " (")
+                .insert(0, e1.getSpecies().getCommonname());
+        }
+        StringBuilder sb2 =
+                new StringBuilder(e2.getSpecies().getScientificname());
+        if (e2.getSpecies().getCommonname() != null){
+            sb2.append(")").insert(0, " (")
+                .insert(0, e2.getSpecies().getCommonname());
+        }
+        subComparisons.put("Species", new StringComparison(
+                sb1.toString(), sb2.toString()));
         subComparisons.put("Function", new StringComparison(
                 e1.getFunction(), e2.getFunction()));
-        subComparisons.put("EC classification",
-                new ListComparison(e1.getEc(), e2.getEc()));
+        subComparisons.put("EC classification", new ListComparison(
+                e1.getEc(), e2.getEc()));
         subComparisons.put("Sequence", new SequenceComparison(
                 e1.getEnzyme().getSequence(), e2.getEnzyme().getSequence()));
     }
