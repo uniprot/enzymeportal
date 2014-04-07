3afa50eacd67b6f2229d937549728655c992456b
Joseph <joseph@ebi.ac.uk>
2014-04-07 15:58:46 +0100

    preparing to release Mega Mapper (testing data release with the new VM Database)

--- enzyme-portal/mega-mapper/scripts/checkParams.sh
+++ enzyme-portal/mega-mapper/scripts/checkParams.sh
@@ -2,5 +2,5 @@
 
 case $1 in
 	enzdev|ezprelvm);;
-	*) echo 'runtime environment (enzdev|ezprel) required as $1' && exit 1;;
+	*) echo 'runtime environment (enzdev|ezprelvm) required as $1' && exit 1;;
 esac
