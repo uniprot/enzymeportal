2f89f029b4ae0604f9b1e4ce0279dbb20beac2e7
Joseph <joseph@ebi.ac.uk>
2014-06-05 09:22:12 +0100

    updated  SitemapGenerator's script

--- enzyme-portal/SitemapGenerator/scripts/sitemap.sh
+++ enzyme-portal/SitemapGenerator/scripts/sitemap.sh
@@ -25,7 +25,7 @@ echo "[INFO] You are about to run the SitemapGenerator using these parameters :"
 echo "[INFO] *******************************************************************"
 echo "[INFO] " $DB_CONFIG $FILE_DIR $FILE_NAME $TEST_MODE
 echo "[INFO] *******************************************************************"
-mvn exec:java -Dexec.mainClass="uk.ac.ebi.ep.sitemap.SiteMapMain" -Dexec.args="$DB_CONFIG $FILE_DIR $FILE_NAME $TEST_MODE"
+mvn exec:java -Dexec.mainClass="uk.ac.ebi.ep.sitemap.main.SiteMapMain" -Dexec.args="$DB_CONFIG $FILE_DIR $FILE_NAME $TEST_MODE"
 echo "[INFO] Running complete -  $(date)"
 echo "About to copy sitemap to this directory /nfs/public/rw/cm/ep/sitemap "
 #become cm_adm cp $FILE_DIR/sitemap-ep*.xml $FILE_DIR/sitemap_index.xml /nfs/public/rw/cm/ep/sitemap
