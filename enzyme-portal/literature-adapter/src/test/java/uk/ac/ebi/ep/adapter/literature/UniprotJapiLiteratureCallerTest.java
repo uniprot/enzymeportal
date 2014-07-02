1371b88a0be88319b7a25f534fc45994d602dd85
Joseph <joseph@ebi.ac.uk>
2014-07-02 11:28:29 +0100

    Incrementing  ep-web-client-1.1.3 to ep-web-client-1.1.4

--- enzyme-portal/literature-adapter/src/test/java/uk/ac/ebi/ep/adapter/literature/UniprotJapiLiteratureCallerTest.java
+++ enzyme-portal/literature-adapter/src/test/java/uk/ac/ebi/ep/adapter/literature/UniprotJapiLiteratureCallerTest.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.ep.adapter.literature;
 
 import java.util.Collection;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import uk.ac.ebi.cdb.webservice.Result;
@@ -15,11 +14,12 @@ public class UniprotJapiLiteratureCallerTest {
 		Collection<Result> citations = caller.call();
 		assertTrue(citations.size() >= 1);
 		Result citation = citations.iterator().next();
-              
+                
+              assertTrue(citation.getAuthorList().getAuthor().size() > 0);
               //This data can change anytime as they are being updated from time to time
-		assertEquals("The DNA sequence and analysis of human chromosome 6.",
-				citation.getTitle());
-		assertEquals("2003", citation.getJournalInfo().getYearOfPublication().toString());
+		//assertEquals("The DNA sequence and analysis of human chromosome 6.",
+				//citation.getTitle());
+		//assertEquals("2003", citation.getJournalInfo().getYearOfPublication().toString());
 //		assertEquals(5, citation.getAuthorList().getAuthor().size());
 //		assertEquals("MED", citation.getSource());
 //		assertEquals("4030726", citation.getId());
