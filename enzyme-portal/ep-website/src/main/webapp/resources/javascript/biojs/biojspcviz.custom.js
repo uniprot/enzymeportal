67f5993fbc054a8e2bf3178680bdb187a369ebae
B. Arman Aksoy <arman@aksoy.org>
2015-02-20 18:16:39 -0500

    added modified files that were forgotten in the earlier commit. Now it is ready for code review/suggestions.

--- enzyme-portal/ep-website/src/main/webapp/resources/javascript/biojs/biojspcviz.custom.js
+++ enzyme-portal/ep-website/src/main/webapp/resources/javascript/biojs/biojspcviz.custom.js
@@ -5,76 +5,84 @@
 */
 $(document).ready(function() {
     $(".pcviz a").click(function(e) {
+        // Let's not load the link and capture the event for custom view
         e.preventDefault();
-        console.log("Gotcha!");
 
-        $("#pcviz-widget").slideDown();
-        $("#pcviz-widget-container").html($("#pcviz-widget-iframe").html());
+        // This is going to be our UniProt ID
+        var uid = $(this).attr("data-uid");
+        var orgUrl = $(this).attr("href");
+
+        // This requires BioJS-PCViz library
+        var biojspcviz = require('biojs-pcviz');
+
+        // Create an instance of PCViz
+        var pcviz = new biojspcviz();
+
+        // This is the HTML element that we are going to fill out based on click events
+        var pcvizDescEl =  $("#pcviz-description");
+
+        // Visualize the neighborhood of uid in a div using this new instance
+        pcviz.neighborhood({
+            el: '#pcviz-widget-container',
+            query: uid,
+            onLoad: function(msg) {
+                // Show overall stats
+                var myContent = "Pathway neighborhood of <b>" + uid + "</b> contains <b>" + msg.numberOfNodes +
+                    " genes</b> and <b>" + msg.numberOfEdges + " interactions</b>. " +
+                    " Click on any of the genes or interactions to see more information. <br><br>" +
+                    "Information provided by <a href='http://www.pathwaycommons.org/pc2/' target='_blank'>Pathway Commons 2</a>.";
+
+                pcvizDescEl.html( myContent );
+                return this;
+            },
+
+            onNodeClick: function(msg)  {
+                var annotation = msg.annotation;
+                var uniprotId = msg.uniprot ? msg.uniprot : annotation.geneUniprotMapping;
+                var summary = msg.uniprotdesc == null ? annotation.geneSummary : info.uniprotdesc;
+                var myContent =  "<table class='grid'>" +
+                    "<tr><th>Gene</th><td>" + msg.id + "</td></tr>" +
+                    "<tr><th>Aliases</th><td>" + annotation.geneAliases.replace(/:/g, ", ") + "</td></tr>" +
+                    "<tr><th>Desciption</th><td>" + summary + "</td></tr>" +
+                    "<tr><th>Uniprot ID</th><td><a target='_blank' href='http://www.ebi.ac.uk/enzymeportal/search/" + uniprotId + "/enzyme'>" + uniprotId + "</a></td></tr>" +
+                    "</table>";
+
+                pcvizDescEl.html( myContent );
+                return this;
+            },
+
+            onEdgeClick: function(msg) {
+                var myContent =  "<table class='grid'>" +
+                    "<tr><th>Interaction</th><td>" + msg.id.replace(/-/g, " ") + "</td></tr>" +
+                    "<tr><th>Publications (PMIDs)</th><td>" + msg.pubmed.join(", ") + "</td></tr>" +
+                    "<tr><th>Participant #1</th><td>" + msg.source + "</td></tr>" +
+                    "<tr><th>Participant #2</th><td>" + msg.target + "</td></tr>" +
+                    "<tr><th>Co-Citations</th><td>" + msg.cited + "</td></tr>" +
+                    "</table>";
+                pcvizDescEl.html( myContent );
+                return this;
+            },
+
+            onFail: function(msg) {
+                // Something went wrong, let's fallback to the default mechanism: open new window
+                $("#pcviz-widget").hide();
+                console.log("Loading PCViz widget failed: " + msg + ". Opening a new window instead.");
+                window.open(orgUrl, "_blank");
+
+                return this;
+            }
+        });
+
     });
 
+    // This is to handle close event by the user
     $("#close-pcviz").click(function(e) {
         e.preventDefault();
         $("#pcviz-widget").slideUp();
     });
 
+    // Hide the widget by default
     $("#pcviz-widget").hide();
 });
 
-
-$(document).ready( function() {
-
-    receiveMessage = function(event) {
-        //if(event.origin !== "http://www.pathwaycommons.org" || event.origin !== "http://awabi.cbio.mskcc.org") { return; }
-
-        // Now let's show the contents in a nice formatted way
-        var msg = JSON.parse( event.data );
-        var msgType = msg.type;
-        var content = msg.content;
-
-        console.log("Message received from the widget:")
-        console.log( msg );
-
-
-        var myEl = $("#pcviz-description").html();
-
-        if(msgType == "pcvizloaded") {
-            myEl = "Pathway neighborhood of <b>PDEBC</b> contains <b>" + content.numberOfNodes + " genes</b> and <b>" + content.numberOfEdges + " interactions</b>. " +
-            " Click on any of the genes or interactions to see more information. <br><br>" +
-            "Information provided by <a href='http://pathwaycommons.org/pc2' target='_blank'>Pathway Commons 2</a>.";
-
-        } else if( msgType == "pcvizclick" ) {
-            var info = content.info;
-
-            switch(content.where) {
-                case "background":
-                    break;
-                case "edge":
-                    var myEl =  "<table class='grid'>" +
-                        "<tr><th>Interaction</th><td>" + info.id.replace(/-/g, " ") + "</td></tr>" +
-                        "<tr><th>Publications (PMIDs)</th><td>" + info.pubmed.join(", ") + "</td></tr>" +
-                        "<tr><th>Participant #1</th><td>" + info.source + "</td></tr>" +
-                        "<tr><th>Participant #2</th><td>" + info.target + "</td></tr>" +
-                        "<tr><th>Co-Citations</th><td>" + info.cited + "</td></tr>" +
-                        "</table>";
-                    break;
-                case "node":
-                    var annotation = info.annotation;
-                    var uniprotId = info.uniprot ? info.uniprot : annotation.geneUniprotMapping;
-                    var summary = info.uniprotdesc == null ? annotation.geneSummary : info.uniprotdesc;
-                    var myEl =  "<table class='grid'>" +
-                        "<tr><th>Gene</th><td>" + info.id + "</td></tr>" +
-                        "<tr><th>Aliases</th><td>" + annotation.geneAliases.replace(/:/g, ", ") + "</td></tr>" +
-                        "<tr><th>Desciption</th><td>" + summary + "</td></tr>" +
-                        "<tr><th>Uniprot ID</th><td><a target='_blank' href='http://www.ebi.ac.uk/enzymeportal/search/" + uniprotId + "/enzyme'>" + uniprotId + "</a></td></tr>" +
-                        "</table>";
-                    break;
-            }
-        }
-
-
-        $("#pcviz-description").html( myEl );
-    };
-
-    window.addEventListener("message", receiveMessage, false);
-});
 
