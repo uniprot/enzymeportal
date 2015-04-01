7ce5e0bb291bfb5a1c0ce243f5d2f7c273f80ef6
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2015-04-01 16:48:26 +0100

    updating ep-parser : 		- config update 	            -  bug fixes 		- added cofactors parser 		- query update 		- unit test update 		- integration test update 		-enhancements

---
+++ enzyme-portal/ep-parser/scripts/cofactor.sh
@@ -0,0 +1,16 @@
+MM_SCRIPTS=$(cd $(dirname $0) && pwd)
+#ensure that db config is passed as param
+. $MM_SCRIPTS/checkParams.sh
+
+DB_CONFIG="$1"
+
+#parse uniprot data for ChEBI Compounds (cofactors)
+echo "[INFO] Request to query & parse UniProt data for ChEBI compounds (Cofactors)  - $(date)"
+echo "[INFO] The dbconfig passed as parameter = " $DB_CONFIG
+echo "[INFO] *******************************************************************"
+WD=$(pwd)
+cd $(dirname $0)/..
+mvn exec:java -Dexec.mainClass="uk.ac.ebi.ep.parser.main.CofactorParser" -Dexec.args="$DB_CONFIG"
+cd $WD
+echo "[INFO] Finished querying and parsing UniProt data and updating Enzyme Portal database with ChEBI Compounds  - $(date)"
+
