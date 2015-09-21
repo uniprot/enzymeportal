e84975e73e7a04b29e7b86420812ee2fd5e67474
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2015-09-21 10:11:49 +0100

    updated ep-parser : 	              -updated memory bsub usage 		 - updated IntenzSaxParser for Rhea reactions

--- enzyme-portal/ep-parser/scripts/submit-one-job-to-farm.sh
+++ enzyme-portal/ep-parser/scripts/submit-one-job-to-farm.sh
@@ -3,7 +3,7 @@
 # password to delete all data and then sends the script enzyme-portal-data.sh to LSF.
 # Param:
 # $1: database environment (uzpdev|uzprel)
-# $2 : the script to be submitted to the farm with extention e.g chebi.sh
+# $2 : the script to be submitted to the farm with extension e.g chebi.sh
 
 echo
 echo "**************************** W A R N I N G ****************************"
@@ -11,4 +11,4 @@ echo "THIS WILL OVERWRITE ALL DATA IN $1. ARE YOU SURE?"
 echo "(Ctrl-C to cancel, Enter to continue)"
 read ok
 
-bsub -R "rusage[mem=64000]" -M 64000 -q production-rh6 $(dirname $0)/$2 $1
+bsub -R "rusage[mem=40000]" -M 40000 -q production-rh6 $(dirname $0)/$2 $1
