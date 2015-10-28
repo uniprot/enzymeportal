d527ade2d05be66fab1f55b19d9a05a81354a479
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2015-10-28 15:19:30 +0000

    Updating SitemapGenerator 		- config update 		- script update

--- enzyme-portal/SitemapGenerator/scripts/publishSitemap.sh
+++ enzyme-portal/SitemapGenerator/scripts/publishSitemap.sh
@@ -29,8 +29,9 @@ echo "[INFO] *******************************************************************
 mvn exec:java -Dexec.mainClass="uk.ac.ebi.ep.sitemap.main.SiteMapMain" -Dexec.args="$DB_CONFIG $FILE_DIR $FILE_NAME $TEST_MODE"
 echo "[INFO] Running complete -  $(date)"
 
-echo "About to copy sitemap to this directory /nfs/public/rw/cm/ep/sitemap "
-become cm_adm cp $FILE_DIR/sitemap-ep*.xml $FILE_DIR/sitemap_index.xml /nfs/public/rw/cm/ep/sitemap
+echo "About to copy sitemap to this directory /nfs/public/rw/uniprot/enzyme_portal/sitemap "
+
+become uni_adm cp $FILE_DIR/sitemap-ep*.xml $FILE_DIR/sitemap_index.xml /nfs/public/rw/uniprot/enzyme_portal/sitemap
 
 echo "Done Copying files. - $(date)"
 echo "deleting the generated files - clean up"
