725d867ec4aeb467f387cb790fc59aba108bad42
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-01-21 14:41:55 +0000

    Releasing epTools-1.0.8 	- added copySitemap.sh 	-script update - generateSitemap only generates sitemaps, use copySitemap to move files around. 	- pom update

---
+++ enzyme-portal/epTools/scripts/copySitemap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Reads dbConfig, file directory and filename from a file.
+# Param:
+# $1: fileDirectory  /ebi/uniprot/production/enzyme_portal/sitemap
+
+SITEMAP_CONFIG=$(dirname $0)/sitemapProp.properties
+FILE_DIR=$(grep '^fileDir=' $SITEMAP_CONFIG | cut -d '=' -f 2)
+
+
+echo "About to copy sitemap to this directory /nfs/public/rw/uniprot/enzyme_portal/sitemap "
+
+become uni_adm cp $FILE_DIR/sitemap-ep*.xml $FILE_DIR/sitemap_index.xml /nfs/public/rw/uniprot/enzyme_portal/sitemap
+
+echo "Done Copying files. - $(date)"
+echo "deleting the generated files - clean up"
+rm -f $FILE_DIR/sitemap*
+echo "Done cleaning up the directory"
+echo "SUCCESS -$(date)"
