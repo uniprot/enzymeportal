73dcfaa850b65fc056a6c9f4a5bd87b99a7e80d9
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-01-21 11:46:22 +0000

    Releasing SitemapGenerator-2.0.7

--- enzyme-portal/SitemapGenerator/scripts/publishSitemap.sh
+++ enzyme-portal/SitemapGenerator/scripts/publishSitemap.sh
@@ -10,7 +10,7 @@
 #EP_CONFIG_DIR=/nfs/panda/production/steinbeck/ep/config
 #SITEMAP_CONFIG=$EP_CONFIG_DIR/ep-mm-db-${1}.properties
 
-SITEMAP_CONFIG=.$(dirname $0)/sitemapProp.properties
+SITEMAP_CONFIG=$(dirname $0)/sitemapProp.properties
 DB_CONFIG=$(grep '^dbConfig=' $SITEMAP_CONFIG | cut -d '=' -f 2)
 FILE_DIR=$(grep '^fileDir=' $SITEMAP_CONFIG | cut -d '=' -f 2)
 FILE_NAME=$(grep '^fileName=' $SITEMAP_CONFIG | cut -d '=' -f 2)
