182b22bd18ab2cb923d52f537172387525f93cad
rantunes <rantunes@ebi.ac.uk>
2016-02-23 09:32:41 +0000

    Add utility class to conduct precondition checks.

---
+++ enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/Preconditions.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.ep.ebeye;
+
+/**
+ * Created by rantunes on 22/02/16.
+ */
+public final class Preconditions {
+    private Preconditions() {}
+
+    public static void checkArgument(boolean precondition, String message) {
+        if(!precondition) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
