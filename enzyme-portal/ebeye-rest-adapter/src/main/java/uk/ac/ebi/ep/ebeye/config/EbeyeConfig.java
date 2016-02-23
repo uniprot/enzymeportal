0c9d1678f90170ded1d0fc59db7b4eebbcc3ae7a
rantunes <rantunes@ebi.ac.uk>
2016-02-23 09:35:01 +0000

    Remove the environment, and replace it with the needed value.
    Add parameter to configure the maximum number of elements per EBeye search response.

--- enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/config/EbeyeConfig.java
+++ enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/config/EbeyeConfig.java
@@ -1,10 +1,10 @@
 package uk.ac.ebi.ep.ebeye.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.AsyncRestTemplate;
@@ -13,18 +13,27 @@ import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.ep.ebeye.EbeyeRestService;
 
 /**
+ * Configures the services that interface with the Ebeye search.
  *
  * @author joseph
  */
 @Configuration
 @PropertySource({"classpath:ebeyeUrl.es"})
 public class EbeyeConfig {
-    @Autowired
-    private Environment env;
+    private static final int MAX_EBI_REQUESTS = 100;
+
+    @Value("${ep.default.search.url}")
+    private String searchUrl;
 
     @Bean
-    public EbeyeRestService ebeyeRestService() {
-        return new EbeyeRestService();
+    static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public EbeyeRestService ebeyeRestService(EbeyeIndexUrl ebeyeIndexUrl, RestTemplate restTemplate,
+            AsyncRestTemplate asyncRestTemplate) {
+        return new EbeyeRestService(ebeyeIndexUrl, restTemplate, asyncRestTemplate, MAX_EBI_REQUESTS);
     }
 
     @Bean
@@ -44,10 +53,7 @@ public class EbeyeConfig {
     @Bean
     public EbeyeIndexUrl ebeyeIndexUrl() {
         EbeyeIndexUrl url = new EbeyeIndexUrl();
-
-        String defaultSearchIndexUrl = env.getProperty("ep.default.search.url");
-        url.setDefaultSearchIndexUrl(defaultSearchIndexUrl);
-
+        url.setDefaultSearchIndexUrl(searchUrl);
         return url;
     }
 }
