b0d7f15b7151b7a7b62f55c8a131455ea1f3cde2
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-03-14 14:56:33 +0000

    updating ep-xml: 	- unit tests update 	- now using global dataSource for H2 in-memory & repository (CRUDE)

--- enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/BaseTest.java
+++ enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/BaseTest.java
@@ -23,7 +23,7 @@ import uk.ac.ebi.ep.data.dataconfig.GlobalConfig;
 import uk.ac.ebi.ep.data.domain.IntenzEnzymes;
 import uk.ac.ebi.ep.data.service.EnzymePortalService;
 import uk.ac.ebi.ep.data.service.EnzymePortalXmlService;
-import uk.ac.ebi.ep.data.testConfig.EnzymeMockDataConfig;
+import uk.ac.ebi.ep.data.testConfig.SpringDataMockConfig;
 import uk.ac.ebi.ep.xml.service.XmlService;
 
 /**
@@ -31,56 +31,64 @@ import uk.ac.ebi.ep.xml.service.XmlService;
  * @author Joseph <joseph@ebi.ac.uk>
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {EnzymeMockDataConfig.class, GlobalConfig.class})
+@ContextConfiguration(classes = {SpringDataMockConfig.class, GlobalConfig.class})
 public abstract class BaseTest extends TestCase {
-
+    
     protected static final org.slf4j.Logger logger = LoggerFactory.getLogger(BaseTest.class);
     @Autowired
     protected EnzymePortalService enzymePortalService;
     @Autowired
     protected EnzymePortalXmlService xmlService;
-
+    
     @Autowired
-    protected DataSource mockDataSource;
-
+    protected DataSource dataSource;
+    
     protected XmlService enzymeCentricServiceInstance;
     protected XmlGenerator enzymeCentricInstance;
     protected XmlService proteinCentricInstance;
     protected List<IntenzEnzymes> enzymes = new ArrayList<>();
-
+    
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
-
+    
     @Rule
     public ExpectedException thrown = ExpectedException.none();
-
+    
     @Before
     @Override
     public void setUp() {
-
+        
         IntenzEnzymes enzyme_1 = new IntenzEnzymes();
         enzyme_1.setInternalId(BigDecimal.ONE);
         enzyme_1.setEcNumber("1.1.1.1");
         enzyme_1.setEnzymeName("alcohol dehydrogenase");
-
+        enzyme_1.setCatalyticActivity(" (1) An alcohol + NAD(+) = an aldehyde or ketone + NADH. (2) A secondary alcohol + NAD(+) = a ketone + NADH.");
+        
         IntenzEnzymes enzyme_2 = new IntenzEnzymes(BigDecimal.valueOf(2L));
         enzyme_2.setEcNumber("6.1.1.1");
         enzyme_2.setEnzymeName("tyrosine—tRNA ligase");
-
+        enzyme_2.setCatalyticActivity("ATP + L-tyrosine + tRNA(Tyr) = AMP + diphosphate + L-tyrosyl-tRNA(Tyr).");
+        
+        IntenzEnzymes enzyme_3 = new IntenzEnzymes(BigDecimal.valueOf(3L));
+        
+        enzyme_3.setEcNumber("3.4.24.85");
+        enzyme_3.setEnzymeName("S2P endopeptidase");
+        
         enzymes.add(enzyme_1);
         enzymes.add(enzyme_2);
-
+        enzymes.add(enzyme_3);
+        
         xmlService.addIntenzEnzymes(enzymes);
-
+        
         enzymeCentricInstance = new EnzymeCentric(enzymePortalService, xmlService);
         proteinCentricInstance = new ProteinCentric(enzymePortalService, xmlService);
     }
-
+    
     protected String resolvePath(String folder) {
         return temporaryFolder
                 .getRoot().toPath()
                 .resolve(folder)
                 .toString();
     }
-
+    
 }
--- enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/EnzymeCentricTest.java
+++ enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/EnzymeCentricTest.java
@@ -26,7 +26,7 @@ public class EnzymeCentricTest extends BaseTest {
     @After
     @Override
     public void tearDown() throws SQLException {
-        mockDataSource.getConnection().close();
+        dataSource.getConnection().close();
         enzymes.clear();
     }
 
@@ -51,18 +51,42 @@ public class EnzymeCentricTest extends BaseTest {
         String fileDir = output.getParent();
         LocalDate date = LocalDate.now();
         String releaseDate = date.format(DateTimeFormatter.ofPattern("d-MMM-uuuu"));
+
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
                 + "<database>\n"
                 + "    <name>Enzyme Portal</name>\n"
                 + "    <description>The Enzyme Portal integrates publicly available information about enzymes, such as small-molecule chemistry, biochemical pathways and drug compounds.</description>\n"
                 + "    <release_date>" + releaseDate + "</release_date>\n"
-                + "    <entry_count>2</entry_count>\n"
+                + "    <entry_count>3</entry_count>\n"
                 + "    <entries>\n"
                 + "        <entry id=\"1.1.1.1\">\n"
                 + "            <name>alcohol dehydrogenase</name>\n"
+                + "            <description> (1) An alcohol + NAD(+) = an aldehyde or ketone + NADH. (2) A secondary alcohol + NAD(+) = a ketone + NADH.</description>\n"
+                + "        </entry>\n"
+                + "        <entry id=\"3.4.24.85\">\n"
+                + "            <name>S2P endopeptidase</name>\n"
+                + "            <additional_fields>\n"
+                + "                <field name=\"uniprot_name\">MBTP2_BOVIN</field>\n"
+                + "                <field name=\"protein_name\">Membrane-bound transcription factor site-2 protease</field>\n"
+                + "                <field name=\"scientific_name\">Bos taurus</field>\n"
+                + "                <field name=\"synonym\">membrane-bound transcription factor site 2 protease</field>\n"
+                + "                <field name=\"synonym\">sterol-regulatory element-binding proteins intramembrane protease</field>\n"
+                + "                <field name=\"synonym\">sterol regulatory element-binding protein site 2 protease</field>\n"
+                + "                <field name=\"synonym\">S2P endopeptidase</field>\n"
+                + "                <field name=\"synonym\">site-2 protease</field>\n"
+                + "                <field name=\"synonym\">-</field>\n"
+                + "                <field name=\"uniprot_name\">MBTP2_MOUSE</field>\n"
+                + "                <field name=\"scientific_name\">Mus musculus</field>\n"
+                + "            </additional_fields>\n"
+                + "            <cross_references>\n"
+                + "                <ref dbkey=\"Q8CHX6\" dbname=\"UNIPROTKB\"/>\n"
+                + "                <ref dbkey=\"3.4.24.85\" dbname=\"INTENZ\"/>\n"
+                + "                <ref dbkey=\"Q0III2\" dbname=\"UNIPROTKB\"/>\n"
+                + "            </cross_references>\n"
                 + "        </entry>\n"
                 + "        <entry id=\"6.1.1.1\">\n"
                 + "            <name>tyrosine—tRNA ligase</name>\n"
+                + "            <description>ATP + L-tyrosine + tRNA(Tyr) = AMP + diphosphate + L-tyrosyl-tRNA(Tyr).</description>\n"
                 + "        </entry>\n"
                 + "    </entries>\n"
                 + "</database>";
@@ -107,13 +131,36 @@ public class EnzymeCentricTest extends BaseTest {
                 + "    <name>Enzyme Portal</name>\n"
                 + "    <description>The Enzyme Portal integrates publicly available information about enzymes, such as small-molecule chemistry, biochemical pathways and drug compounds.</description>\n"
                 + "    <release_date>" + releaseDate + "</release_date>\n"
-                + "    <entry_count>2</entry_count>\n"
+                + "    <entry_count>3</entry_count>\n"
                 + "    <entries>\n"
                 + "        <entry id=\"1.1.1.1\">\n"
                 + "            <name>alcohol dehydrogenase</name>\n"
+                + "            <description> (1) An alcohol + NAD(+) = an aldehyde or ketone + NADH. (2) A secondary alcohol + NAD(+) = a ketone + NADH.</description>\n"
+                + "        </entry>\n"
+                + "        <entry id=\"3.4.24.85\">\n"
+                + "            <name>S2P endopeptidase</name>\n"
+                + "            <additional_fields>\n"
+                + "                <field name=\"uniprot_name\">MBTP2_BOVIN</field>\n"
+                + "                <field name=\"protein_name\">Membrane-bound transcription factor site-2 protease</field>\n"
+                + "                <field name=\"scientific_name\">Bos taurus</field>\n"
+                + "                <field name=\"synonym\">membrane-bound transcription factor site 2 protease</field>\n"
+                + "                <field name=\"synonym\">sterol-regulatory element-binding proteins intramembrane protease</field>\n"
+                + "                <field name=\"synonym\">sterol regulatory element-binding protein site 2 protease</field>\n"
+                + "                <field name=\"synonym\">S2P endopeptidase</field>\n"
+                + "                <field name=\"synonym\">site-2 protease</field>\n"
+                + "                <field name=\"synonym\">-</field>\n"
+                + "                <field name=\"uniprot_name\">MBTP2_MOUSE</field>\n"
+                + "                <field name=\"scientific_name\">Mus musculus</field>\n"
+                + "            </additional_fields>\n"
+                + "            <cross_references>\n"
+                + "                <ref dbkey=\"Q8CHX6\" dbname=\"UNIPROTKB\"/>\n"
+                + "                <ref dbkey=\"3.4.24.85\" dbname=\"INTENZ\"/>\n"
+                + "                <ref dbkey=\"Q0III2\" dbname=\"UNIPROTKB\"/>\n"
+                + "            </cross_references>\n"
                 + "        </entry>\n"
                 + "        <entry id=\"6.1.1.1\">\n"
                 + "            <name>tyrosine—tRNA ligase</name>\n"
+                + "            <description>ATP + L-tyrosine + tRNA(Tyr) = AMP + diphosphate + L-tyrosyl-tRNA(Tyr).</description>\n"
                 + "        </entry>\n"
                 + "    </entries>\n"
                 + "</database>";
@@ -121,7 +168,6 @@ public class EnzymeCentricTest extends BaseTest {
         assertThat(output).hasExtension("xml")
                 .hasContent(xml.trim())
                 .hasParent(resolvePath(fileDir));
-
     }
 
 }
