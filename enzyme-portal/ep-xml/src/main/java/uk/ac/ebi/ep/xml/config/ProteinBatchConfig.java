4f2ab187324ec058d59a5516af33795bd31b644e
rantunes <rantunes@ebi.ac.uk>
2016-03-21 16:22:03 +0000

    Add Java config to setup the protein centric XML generation.

---
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/config/ProteinBatchConfig.java
@@ -0,0 +1,142 @@
+package uk.ac.ebi.ep.xml.config;
+
+import uk.ac.ebi.ep.data.domain.UniprotEntry;
+import uk.ac.ebi.ep.xml.generator.protein.ProteinXmlFooterCallback;
+import uk.ac.ebi.ep.xml.generator.protein.ProteinXmlHeaderCallback;
+import uk.ac.ebi.ep.xml.generator.protein.UniProtEntryToEntryConverter;
+import uk.ac.ebi.ep.xml.model.Entry;
+
+import javax.persistence.EntityManagerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+/**
+ * Configures the protein centric XML generator to use Spring batch.
+ */
+@Configuration
+@EnableBatchProcessing
+@PropertySource(value = "classpath:ep-xml-config.properties", ignoreResourceNotFound = true)
+public class ProteinBatchConfig extends DefaultBatchConfigurer {
+    public static final String PROTEIN_CENTRIC_JOB = "PROTEIN_CENTRIC_JOB";
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JobBuilderFactory jobBuilders;
+
+    @Autowired
+    private StepBuilderFactory stepBuilders;
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public String releaseNumber() {
+        return env.getProperty("release.number");
+    }
+
+    @Bean
+    public Resource proteinCentricXmlDir() {
+        return new FileSystemResource(env.getProperty("ep.protein.centric.xml.dir"));
+    }
+
+    @Bean
+    public JobLauncher jobLauncher() {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(this.getJobRepository());
+
+        return jobLauncher;
+    }
+
+    @Bean
+    public Job proteinCentricJob() {
+        return jobBuilders.get("PROTEIN_CENTRIC_JOB")
+                .start(readFromDbWriteToXMLStep())
+                .build();
+    }
+
+    @Bean
+    public Step readFromDbWriteToXMLStep() {
+        return stepBuilders.get("readFromDbWriteToXMLStep")
+                .<UniprotEntry, Entry>chunk(chunkSize())
+                .<UniprotEntry>reader(uniProtEntryReader())
+                .processor(uniProtEntryToEntryConverter())
+                .writer(entryToXmlWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemReader<UniprotEntry> uniProtEntryReader() {
+        JpaPagingItemReader<UniprotEntry> databaseReader = new JpaPagingItemReader<>();
+        databaseReader.setEntityManagerFactory(entityManagerFactory);
+        databaseReader.setQueryString("select u from UniprotEntry u");
+        databaseReader.setPageSize(chunkSize());
+
+        return databaseReader;
+    }
+
+    @Bean
+    public ItemProcessor<UniprotEntry, Entry> uniProtEntryToEntryConverter() {
+        return new UniProtEntryToEntryConverter();
+    }
+
+    @Bean
+    public ItemWriter<Entry> entryToXmlWriter() {
+        StaxEventItemWriter<Entry> xmlWriter = new StaxEventItemWriter<>();
+        xmlWriter.setResource(proteinCentricXmlDir());
+        xmlWriter.setRootTagName("database");
+        xmlWriter.setMarshaller(entryMarshaller());
+        xmlWriter.setHeaderCallback(new ProteinXmlHeaderCallback(releaseNumber()));
+        xmlWriter.setFooterCallback(new ProteinXmlFooterCallback());
+
+        return xmlWriter;
+    }
+
+    /**
+     * Creates a job repository that uses an in memory map to register the job's progress. This should be changed to
+     * use a real data source in the following cases:
+     *    - If you want to be able to resume a failed job
+     *    - If you want more than one of the same job (with the same parameters) to be launched simultaneously
+     *    - If you want to multi thread the job
+     *    - If you have a locally partitioned step.
+     *
+     * @return a JobRepository
+     * @throws Exception if an error is encountered whilst creating the job repo
+     */
+    @Override
+    protected JobRepository createJobRepository() throws Exception {
+        MapJobRepositoryFactoryBean factory = new MapJobRepositoryFactoryBean();
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    private Marshaller entryMarshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setClassesToBeBound(Entry.class);
+        return marshaller;
+    }
+
+    private int chunkSize() {
+        return Integer.parseInt(env.getProperty("ep.protein.centric.chunk"));
+    }
+}
