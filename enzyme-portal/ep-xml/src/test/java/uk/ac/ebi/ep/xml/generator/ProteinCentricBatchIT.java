95b1233a89297da5e86b8ba0669f62a0283e1b24
rantunes <rantunes@ebi.ac.uk>
2016-03-22 11:39:07 +0000

    Add junit rule to cleanup generated XML file. Add code to check how many entries should be processed.

--- enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/ProteinCentricBatchIT.java
+++ enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/ProteinCentricBatchIT.java
@@ -3,7 +3,13 @@ package uk.ac.ebi.ep.xml.generator;
 import uk.ac.ebi.ep.data.testConfig.SpringDataMockConfig;
 import uk.ac.ebi.ep.xml.config.ProteinBatchConfig;
 
-import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
@@ -20,11 +26,32 @@ import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
 @ContextConfiguration(
         classes = {ProteinBatchConfig.class, JobTestRunnerConfig.class, SpringDataMockConfig.class})
 public class ProteinCentricBatchIT {
+    @ClassRule
+    public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeClass
+    public static void setupXmlFileLocation() throws IOException {
+        File xmlFile = temporaryFolder.newFile();
+        System.setProperty("ep.protein.centric.xml.dir", xmlFile.getAbsolutePath());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
     @Test
     public void successfulJobRun() throws Exception {
+        int expectedEntries = countEntries();
+
         JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 
         BatchStatus status = jobExecution.getStatus();
@@ -32,8 +59,8 @@ public class ProteinCentricBatchIT {
 
         StepExecution step = getStepByName(ProteinBatchConfig.PROTEIN_CENTRIC_DB_TO_XML_STEP, jobExecution);
 
-        assertThat(step.getReadCount(), is(17));
-        assertThat(step.getWriteCount(), is(17));
+        assertThat(step.getReadCount(), is(expectedEntries));
+        assertThat(step.getWriteCount(), is(expectedEntries));
         assertThat(step.getSkipCount(), is(0));
     }
 
@@ -45,5 +72,11 @@ public class ProteinCentricBatchIT {
         }
 
         throw new IllegalArgumentException("Step name not recognized: " + stepName);
+    }
+
+    private int countEntries() {
+        Query query = entityManager.createQuery("select count(u.dbentryId) from UniprotEntry u");
+
+        return ((Long) query.getSingleResult()).intValue();
     }
 }
