58dc704a4abd76168e077202216f62387991e84a
rantunes <rantunes@ebi.ac.uk>
2016-03-22 16:11:31 +0000

    Change code to use the ModelDateUtil halper class to convert a date into its correct string representation.

--- enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallback.java
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallback.java
@@ -1,10 +1,10 @@
 package uk.ac.ebi.ep.xml.generator.protein;
 
 import uk.ac.ebi.ep.xml.generator.XmlTransformer;
+import uk.ac.ebi.ep.xml.model.ModelDateUtil;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLStreamException;
@@ -23,6 +23,12 @@ import org.springframework.batch.item.xml.StaxWriterCallback;
  * @author Ricardo Antunes
  */
 public class ProteinXmlHeaderCallback implements StaxWriterCallback {
+    final static String DB_NAME_ELEMENT = "name";
+    final static String DB_DESCRIPTION_ELEMENT = "description";
+    final static String RELEASE_VERSION_ELEMENT = "release";
+    final static String RELEASE_DATE_ELEMENT = "release_date";
+    final static String ENTRY_COUNT_ELEMENT = "entry_count";
+
     private final String release;
 
     public ProteinXmlHeaderCallback(String release) {
@@ -33,11 +39,11 @@ public class ProteinXmlHeaderCallback implements StaxWriterCallback {
         try {
             XMLEventFactory eventFactory = XMLEventFactory.newInstance();
             //TODO move names to somewhere else
-            appendTag("name", XmlTransformer.enzymePortal, eventFactory, writer);
-            appendTag("description", XmlTransformer.enzymePortalDescription, eventFactory, writer);
-            appendTag("release", release, eventFactory, writer);
-            appendTag("release_date", createTodayDate(), eventFactory, writer);
-            appendTag("entry_count", "0", eventFactory, writer); //TODO calculate total number of entries
+            appendTag(DB_NAME_ELEMENT, XmlTransformer.enzymePortal, eventFactory, writer);
+            appendTag(DB_DESCRIPTION_ELEMENT, XmlTransformer.enzymePortalDescription, eventFactory, writer);
+            appendTag(RELEASE_VERSION_ELEMENT, release, eventFactory, writer);
+            appendTag(RELEASE_DATE_ELEMENT, createTodayDate(), eventFactory, writer);
+            appendTag(ENTRY_COUNT_ELEMENT, "0", eventFactory, writer); //TODO calculate total number of entries
 
             XMLEvent event = eventFactory.createStartElement("", "", "entries");
             writer.add(event);
@@ -54,8 +60,7 @@ public class ProteinXmlHeaderCallback implements StaxWriterCallback {
         writer.add(eventFactory.createEndElement("", "", tag));
     }
 
-    //TODO: find a way to reuse the date format defined in the data adaptor
     private String createTodayDate() {
-        return LocalDate.now().format(DateTimeFormatter.ofPattern("d-MMM-uuuu"));
+        return ModelDateUtil.convertDateToString(LocalDate.now());
     }
 }
