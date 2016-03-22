0671f5ce37629b696dffab8abc8149196129e206
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-03-22 15:05:49 +0000

    Updating ep-data : 	-repository updates (UniprotRepository) 	- service updates (XMLService), EnzymePortalService

--- enzyme-portal/enzyme-portal-data/src/main/java/uk/ac/ebi/ep/data/service/EnzymePortalService.java
+++ enzyme-portal/enzyme-portal-data/src/main/java/uk/ac/ebi/ep/data/service/EnzymePortalService.java
@@ -830,51 +830,6 @@ public class EnzymePortalService {
         uniprotEntryRepository.updateExpEvidenceFlag();
     }
 
-    private static Predicate swissprotEnzymesByEcNumber(String ecNumber) {
-        QUniprotEntry enzyme = QUniprotEntry.uniprotEntry;
 
-        return enzyme.entryType.eq(Integer.valueOf(0).shortValue()).and(enzyme.enzymePortalEcNumbersSet.any().ecNumber.equalsIgnoreCase(ecNumber));
-
-    }
-
-    @Transactional(readOnly = true)
-    public Page<UniprotEntry> findPageableUniprotEntries(Pageable pageable) {
-
-        return uniprotEntryRepository.findAll(pageable);
-    }
-
-    @Transactional(readOnly = true)
-    public List<UniprotEntry> findUniprotEntriesOrderedByEntryType() {
-
-        return uniprotEntryRepository.findUniprotEntriesOrderedByEntryType();
-    }
-    
-        @Transactional(readOnly = true)
-    public List<UniprotEntry> findUniprotEntries() {
-
-        return uniprotEntryRepository.findUniprotEntries();
-    }
-
-    @Transactional(readOnly = true)
-    public Iterable<UniprotEntry> findSwissprotEnzymesByEcNumber(String ec) {
-
-        return uniprotEntryRepository.findAll(swissprotEnzymesByEcNumber(ec));
-    }
-
-    @Transactional(readOnly = true)
-    public List<UniprotEntry> findSwissprotEnzymesByEc(String ec) {
-
-        return uniprotEntryRepository.findSwissprotEnzymesByEc(ec);
-    }
-
-    @Transactional(readOnly = true)
-    public Stream<List<UniprotEntry>> findStreamedSwissprotEnzymesByEc(String ec) {
-        return uniprotEntryRepository.findStreamedSwissprotEnzymesByEc(ec);
-    }
-
-    @Transactional(readOnly = true)
-    public Stream<UniprotEntry> streamEnzymes() {
-        return uniprotEntryRepository.streamEnzymes();
-    }
 
 }
