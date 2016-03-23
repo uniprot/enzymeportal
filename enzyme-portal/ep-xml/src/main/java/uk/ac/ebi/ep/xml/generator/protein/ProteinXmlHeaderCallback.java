4ec903a8c05feb8d42fa1fd71c7727aba463bbdf
rantunes <rantunes@ebi.ac.uk>
2016-03-23 10:43:42 +0000

    Add EnzymePortalXmlService to get the number of UniProt entries to process.

--- enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallback.java
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallback.java
@@ -1,8 +1,10 @@
 package uk.ac.ebi.ep.xml.generator.protein;
 
+import uk.ac.ebi.ep.data.service.EnzymePortalXmlService;
 import uk.ac.ebi.ep.xml.generator.XmlTransformer;
 import uk.ac.ebi.ep.xml.model.ModelDateUtil;
 
+import com.google.common.base.Preconditions;
 import java.io.IOException;
 import java.time.LocalDate;
 import javax.xml.stream.XMLEventFactory;
@@ -29,38 +31,56 @@ public class ProteinXmlHeaderCallback implements StaxWriterCallback {
     final static String RELEASE_DATE_ELEMENT = "release_date";
     final static String ENTRY_COUNT_ELEMENT = "entry_count";
 
-    private final String release;
+    final static String ENTRIES_ELEMENT = "entries";
 
-    public ProteinXmlHeaderCallback(String release) {
+    private final String release;
+    private final EnzymePortalXmlService enzymePortalService;
+
+    public ProteinXmlHeaderCallback(String release, EnzymePortalXmlService enzymePortalService) {
+        Preconditions.checkArgument(enzymePortalService != null, "Enzyme portal XML service can not be null");
+        Preconditions.checkArgument(release != null, "Release version can not be null");
+
         this.release = release;
+        this.enzymePortalService = enzymePortalService;
     }
 
     @Override public void write(XMLEventWriter writer) throws IOException {
         try {
             XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 
-            appendTag(DB_NAME_ELEMENT, XmlTransformer.enzymePortal, eventFactory, writer);
-            appendTag(DB_DESCRIPTION_ELEMENT, XmlTransformer.enzymePortalDescription, eventFactory, writer);
-            appendTag(RELEASE_VERSION_ELEMENT, release, eventFactory, writer);
-            appendTag(RELEASE_DATE_ELEMENT, createTodayDate(), eventFactory, writer);
-            appendTag(ENTRY_COUNT_ELEMENT, "0", eventFactory, writer); //TODO calculate total number of entries
+            appendTagWithValue(DB_NAME_ELEMENT, XmlTransformer.ENZYME_PORTAL, eventFactory, writer);
+            appendTagWithValue(DB_DESCRIPTION_ELEMENT, XmlTransformer.ENZYME_PORTAL_DESCRIPTION, eventFactory, writer);
+            appendTagWithValue(RELEASE_VERSION_ELEMENT, release, eventFactory, writer);
+            appendTagWithValue(RELEASE_DATE_ELEMENT, createTodayDate(), eventFactory, writer);
+            appendTagWithValue(ENTRY_COUNT_ELEMENT, queryNumberOfUniProtEntries(), eventFactory, writer);
 
-            XMLEvent event = eventFactory.createStartElement("", "", "entries");
+            XMLEvent event = eventFactory.createStartElement("", "", ENTRIES_ELEMENT);
             writer.add(event);
-
         } catch (XMLStreamException ex) {
             throw new RuntimeException("Unable to write the header on the XML file");
         }
     }
 
-    private void appendTag(String tag, String name, XMLEventFactory eventFactory, XMLEventWriter writer)
+    private void appendTagWithValue(String tag, String value, XMLEventFactory eventFactory, XMLEventWriter writer)
             throws XMLStreamException {
         writer.add(eventFactory.createStartElement("", "", tag));
-        writer.add(eventFactory.createCharacters(name));
+        writer.add(eventFactory.createCharacters(value));
         writer.add(eventFactory.createEndElement("", "", tag));
     }
 
     private String createTodayDate() {
         return ModelDateUtil.convertDateToString(LocalDate.now());
+    }
+
+    private String queryNumberOfUniProtEntries() {
+        String totalText = "0";
+
+        Long total = enzymePortalService.countUniprotEntries();
+
+        if(total != null) {
+            totalText = String.valueOf(total);
+        }
+
+        return totalText;
     }
 }
--- enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallbackTest.java
+++ enzyme-portal/ep-xml/src/test/java/uk/ac/ebi/ep/xml/generator/protein/ProteinXmlHeaderCallbackTest.java
@@ -1,35 +1,46 @@
 package uk.ac.ebi.ep.xml.generator.protein;
 
+import uk.ac.ebi.ep.data.service.EnzymePortalXmlService;
 import uk.ac.ebi.ep.xml.generator.XmlTransformer;
 import uk.ac.ebi.ep.xml.model.ModelDateUtil;
 
 import java.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.w3c.dom.Document;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasXPath;
+import static org.mockito.Mockito.when;
 import static uk.ac.ebi.ep.xml.generator.protein.ProteinXmlHeaderCallback.*;
 
 /**
  * Tests the behaviour of the {@link ProteinXmlHeaderCallback} class
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ProteinXmlHeaderCallbackTest extends AbstractCallbackSetup {
     private static final String ROOT_ELEMENT_NAME = "database";
 
     private static final String RELEASE_VERSION = "rel1";
+    private static final String DB_NAME = XmlTransformer.ENZYME_PORTAL;
+    private static final String DB_DESCRIPTION = XmlTransformer.ENZYME_PORTAL_DESCRIPTION;
 
     private static final String ELEMENT_OCCURS_N_TIMES_XPATH = "%s[count(%s)=%d]";
     private static final String MESSAGE_OCCURS_IN_ELEMENT_XPATH = "%s/%s[.='%s']";
 
     private ProteinXmlHeaderCallback headerCallback;
 
+    @Mock
+    private EnzymePortalXmlService enzymeService;
+
     @Override @Before
     public void setUp() throws Exception {
         super.setUp();
 
-        headerCallback = new ProteinXmlHeaderCallback(RELEASE_VERSION);
+        headerCallback = new ProteinXmlHeaderCallback(RELEASE_VERSION, enzymeService);
     }
 
     @Test
@@ -41,7 +52,7 @@ public class ProteinXmlHeaderCallbackTest extends AbstractCallbackSetup {
         Document xmlDoc = parse(xmlOutput);
 
         assertElementAppearsNTimesInDoc(DB_NAME_ELEMENT, 1, xmlDoc);
-        assertElementHasMessageInDoc(DB_NAME_ELEMENT, XmlTransformer.enzymePortal, xmlDoc);
+        assertElementHasMessageInDoc(DB_NAME_ELEMENT, DB_NAME, xmlDoc);
     }
 
     @Test
@@ -51,7 +62,7 @@ public class ProteinXmlHeaderCallbackTest extends AbstractCallbackSetup {
         Document xmlDoc = createXmlDocument(output.toString());
 
         assertElementAppearsNTimesInDoc(DB_DESCRIPTION_ELEMENT, 1, xmlDoc);
-        assertElementHasMessageInDoc(DB_DESCRIPTION_ELEMENT, XmlTransformer.enzymePortalDescription, xmlDoc);
+        assertElementHasMessageInDoc(DB_DESCRIPTION_ELEMENT, DB_DESCRIPTION, xmlDoc);
     }
 
     @Test
@@ -78,12 +89,25 @@ public class ProteinXmlHeaderCallbackTest extends AbstractCallbackSetup {
 
     @Test
     public void headerContainsEntryCountElement() throws Exception {
+        long totalCount = 17;
+
+        when(enzymeService.countUniprotEntries()).thenReturn(totalCount);
+
         headerCallback.write(writer);
 
         Document xmlDoc = createXmlDocument(output.toString());
 
         assertElementAppearsNTimesInDoc(ENTRY_COUNT_ELEMENT, 1, xmlDoc);
-        assertElementHasMessageInDoc(ENTRY_COUNT_ELEMENT, "0", xmlDoc);
+        assertElementHasMessageInDoc(ENTRY_COUNT_ELEMENT, String.valueOf(totalCount), xmlDoc);
+    }
+
+    @Test
+    public void headerContainsEntriesStartElement() throws Exception {
+        headerCallback.write(writer);
+
+        Document xmlDoc = createXmlDocument(output.toString());
+
+        assertElementAppearsNTimesInDoc(ENTRY_COUNT_ELEMENT, 1, xmlDoc);
     }
 
     private Document createXmlDocument(String toConvert) throws Exception {
@@ -91,7 +115,9 @@ public class ProteinXmlHeaderCallbackTest extends AbstractCallbackSetup {
     }
 
     private String wrapWithRootElement(String xml) {
-        return "<" + ROOT_ELEMENT_NAME + ">" + xml + "</" + ROOT_ELEMENT_NAME + ">";
+        //entries element is left open by xmlEventWriter, so it is necessary to close the element.
+        String entriesClosingElement = "/>";
+        return "<" + ROOT_ELEMENT_NAME + ">" + xml + entriesClosingElement + "</" + ROOT_ELEMENT_NAME + ">";
     }
 
     private void assertElementAppearsNTimesInDoc(String elementName, int occurrence, Document xmlDoc) {
