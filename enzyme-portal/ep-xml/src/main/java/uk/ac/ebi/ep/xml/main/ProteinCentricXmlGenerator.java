50fb52c924295c4ac967eb807f404755ff0936e2
rantunes <rantunes@ebi.ac.uk>
2016-03-23 15:11:38 +0000

    Change class to startup the protein centric job correctly.

--- enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/main/ProteinCentricXmlGenerator.java
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/main/ProteinCentricXmlGenerator.java
@@ -2,13 +2,20 @@ package uk.ac.ebi.ep.xml.main;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.configuration.JobLocator;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.Environment;
 
 import uk.ac.ebi.ep.data.dataconfig.DataConfig;
 import uk.ac.ebi.ep.xml.config.ProteinBatchConfig;
 import uk.ac.ebi.ep.xml.util.Preconditions;
+import uk.ac.ebi.ep.xml.validator.EnzymePortalXmlValidator;
 
 /**
  *
@@ -25,15 +32,32 @@ public final class ProteinCentricXmlGenerator {
         profile = args[0];
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.registerShutdownHook();
         context.getEnvironment().setActiveProfiles(profile);
         context.register(DataConfig.class, ProteinBatchConfig.class);
+        context.scan("uk.ac.ebi.ep.data.dataconfig");
         context.refresh();
 
-        JobLocator jobLocator = context.getBean(JobLocator.class);
+        runXmlGeneration(context);
+        validateXml(context);
+    }
 
-        Job job = jobLocator.getJob(ProteinBatchConfig.PROTEIN_CENTRIC_JOB);
+    private static void runXmlGeneration(ApplicationContext context)
+            throws NoSuchJobException, JobParametersInvalidException, JobExecutionAlreadyRunningException,
+                   JobRestartException, JobInstanceAlreadyCompleteException {
+        Job proteinCentricJob = context.getBean("proteinCentricJob", Job.class);
 
         JobLauncher launcher = context.getBean(JobLauncher.class);
-        launcher.run(job, new JobParameters());
+        launcher.run(proteinCentricJob, new JobParameters());
+    }
+
+    private static void validateXml(ApplicationContext context) {
+        Environment env = context.getEnvironment();
+
+        String xmlFile = env.getProperty("ep.protein.centric.xml.dir");
+        String[] ebeyeXSDs = env.getProperty("ep.ebeye.xsd").split(",");
+
+
+        EnzymePortalXmlValidator.validateXml(xmlFile, ebeyeXSDs);
     }
 }
