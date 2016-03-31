03ce4d16c71dcd4fb25aef8827b5ff5c55b1a683
rantunes <rantunes@ebi.ac.uk>
2016-03-31 12:07:16 +0100

    Reduce max size of collection that stores the accessions when NO_LIMIT option is chosen.
    The NO_LIMIT value is currently Integer.MAX_VALUE if the client doesn't want a limit, then this is the option he will choose. However this value dictates the size of the collection that holds the accession. We can not initialize this to Integer.MAX_VALUE, because its to big. The collection is now reduced to the hit count size if this is smaller than the limit.

--- enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/EbeyeRestService.java
+++ enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/EbeyeRestService.java
@@ -132,7 +132,7 @@ public class EbeyeRestService {
 
         logger.debug("Possible number of paginated queries: {}", totalPaginatedQueries);
 
-        Set<String> uniqueAccessions = new LinkedHashSet<>(limit);
+        Set<String> uniqueAccessions = new LinkedHashSet<>(limit > hitCount ? hitCount : limit);
 
         int startPage = 0;
         int endPage = Math.min(chunkSize, totalPaginatedQueries);
