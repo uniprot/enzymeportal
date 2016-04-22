03281688eca41fdfda41e3622b064cdad3a251fc
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-04-22 17:22:16 +0100

    Updating ep-tools: 	-script update

--- enzyme-portal/epTools/scripts/parser/submitOneLFS.sh
+++ enzyme-portal/epTools/scripts/parser/submitOneLFS.sh
@@ -11,5 +11,5 @@ echo "THIS WILL OVERWRITE ALL DATA IN $1. ARE YOU SURE?"
 echo "(Ctrl-C to cancel, Enter to continue)"
 read ok
 
-bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$2 $1
+bsub -U enzyme-portal@ebi.ac.uk -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$2 $1
 #bsub -q production-rh6 $(dirname $0)/$2 $1
