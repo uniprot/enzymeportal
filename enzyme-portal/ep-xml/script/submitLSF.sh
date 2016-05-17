ab231a74d9a28d857b6ebb2a56ec7d2c282725bb
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-05-17 12:26:48 +0100

    updating ep-xml : 		- script update

--- enzyme-portal/ep-xml/script/submitLSF.sh
+++ enzyme-portal/ep-xml/script/submitLSF.sh
@@ -4,15 +4,16 @@
 #usage : ./submitLSF.sh <the script>  DB_CONFIG example : ./submitLSF.sh protein-xml-generator.sh uzprel
 #todo fix issue : joseph@ebi-001 $ bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 ./enzyme-xml-generator.sh uzprel
 # Param:
-# $1: database environment (uzpdev|uzprel)
-# $2 : the script to be submitted to the farm with extension e.g protein-xml-generator.sh
+# $2: database environment (uzpdev|uzprel)
+# $1 : the script to be submitted to the farm with extension e.g protein-xml-generator.sh
 
 echo
 echo "**************************** W A R N I N G ****************************"
-echo "THIS WILL RUN THIS SCRIPT $2 AGAINST THE DATABASE $1. DO YOU WANT TO CONTINUE?"
+echo "THIS WILL RUN THIS SCRIPT $1 AGAINST THE DATABASE $2. DO YOU WANT TO CONTINUE?"
 echo "(Ctrl-C to cancel, Enter to continue)"
 read ok
 
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$2 $1
+#bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$1 $2
 
-bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $2 $1
+bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $1 $2
