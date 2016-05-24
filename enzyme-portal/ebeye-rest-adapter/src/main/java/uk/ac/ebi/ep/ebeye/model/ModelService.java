94e5fe2b9149bdf85d69641afffa1aec0847a618
Joseph <joseph@c02hq0ladv16.windows.ebi.ac.uk>
2016-05-24 15:40:48 +0100

    Updating ep-website: 	- pom update 	- applying spring pagination

--- enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/model/ModelService.java
+++ enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/model/ModelService.java
@@ -23,13 +23,13 @@ public class ModelService {
         EBISearchResult results = restTemplate.getForObject(url.trim(), EBISearchResult.class);
         return results;
     }
-    
-    public EBISearchResult filterSearchResult(String query, int page,String facets) {
+
+    public EBISearchResult filterSearchResult(String query, int page, String facets) {
 //        System.out.println("facets "+ facets);
 //String u = "http://www.ebi.ac.uk/ebisearch/ws/rest/enzymeportal_enzymes?query=kinase&facetcount=10&facets=TAXONOMY:9606,disease_name:Leigh%20syndrome,disease_name:Glioma%202"; 
-        String url = "http://www.ebi.ac.uk/ebisearch/ws/rest/enzymeportal_enzymes?query=" + query + "&facetcount=15&facets="+facets+"&start=" + page + "&size=20&fields=id,name,description,UNIPROTKB,protein_name,common_name,scientific_name,compound_name,disease_name,enzyme_family&format=json";
+        String url = "http://www.ebi.ac.uk/ebisearch/ws/rest/enzymeportal_enzymes?query=" + query + "&facetcount=15&facets=" + facets + "&start=" + page + "&size=20&fields=id,name,description,UNIPROTKB,protein_name,common_name,scientific_name,compound_name,disease_name,enzyme_family&format=json";
         EBISearchResult results = restTemplate.getForObject(url.trim(), EBISearchResult.class);
-        return results;  
+        return results;
     }
 
     public List<Entry> getEnzymeView(String query, int page) {
