9292bef965fd18404c68c85e8a6379f7a6023fd5
Joseph <joseph@computingfacts.com>
2017-09-25 16:21:15 +0100

    updating ep-website : 	- config update 	- code refactoring 	- pom update

--- enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/config/ProteinGroupsBatchConfig.java
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/config/ProteinGroupsBatchConfig.java
@@ -34,12 +34,15 @@ public class ProteinGroupsBatchConfig extends AbstractBatchConfig<ProteinGroups,
     private static final String COUNT_QUERY = "select count(p.proteinGroupId) from ProteinGroups p";
     private static final String ROOT_TAG_NAME = "database";
     // private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS";
-   // private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE PROTEIN_GROUP_ID='E76XC1'";
+    //private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE PROTEIN_GROUP_ID='E76XC1'";
     // private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE ENTRY_TYPE=0 AND ROWNUM <= 5";
     private static final String NATIVE_QUERY = "select * from PROTEIN_GROUPS where ENTRY_TYPE=0 and rownum<=1000\n"
             + "union\n"
             + "select * from PROTEIN_GROUPS where ENTRY_TYPE=1 and rownum<=500 ";
 
+       //private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE PROTEIN_GROUP_ID='EJLX47'";
+    
+    
     @Bean
     @Override
     public Resource proteinCentricXmlDir() {
