65ef17281110ee8cc80240668a763990ee6a7e18
Joseph <joseph@computingfacts.com>
2017-10-11 14:34:19 +0100

    updating ebeye rest service : 	- code refactoring 	- added catalytic activities field

--- enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/ProteinGroupService.java
+++ enzyme-portal/ebeye-rest-adapter/src/main/java/uk/ac/ebi/ep/ebeye/ProteinGroupService.java
@@ -56,12 +56,12 @@ public class ProteinGroupService extends ProteinQueryServiceImpl {
     //Example of filter by facet
     //http://wwwdev.ebi.ac.uk/ebisearch/ws/rest/enzymeportal_proteins?query=human&facetcount=10&facets=cofactor:18420,OMIM:612740&format=xml
     private String buildQueryUrl(String endpoint, String query, int facetCount, String facets, int startPage, int pageSize) {
-        String ebeyeQueryUrl = "%s?query=%s&facetcount=%d&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,ec,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym&sort=entry_type:ascending&format=json";
+        String ebeyeQueryUrl = "%s?query=%s&facetcount=%d&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,ec,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym,catalytic_activity&sort=entry_type:ascending&format=json";
         //String ebeyeQueryUrl = "%s?query=%s&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,common_name,scientific_name,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym&sort=_relevance&reverse=true&format=json";
 
         if (!StringUtils.isEmpty(facets) && StringUtils.hasText(facets)) {
 
-            ebeyeQueryUrl = "%s?query=%s&facetcount=%d&facets=%s&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,ec,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym&sort=entry_type:ascending&format=json";
+            ebeyeQueryUrl = "%s?query=%s&facetcount=%d&facets=%s&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,ec,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym,catalytic_activity&sort=entry_type:ascending&format=json";
             return String.format(ebeyeQueryUrl, endpoint, query, facetCount, facets, startPage, pageSize);
             //ebeyeQueryUrl = "%s?query=%s&start=%d&size=%d&fields=id,primary_organism,primary_accession,name,common_name,scientific_name,entry_type,gene_name,primary_image,function,related_species,disease_name,synonym&sort=_relevance&reverse=true&format=json";
 
