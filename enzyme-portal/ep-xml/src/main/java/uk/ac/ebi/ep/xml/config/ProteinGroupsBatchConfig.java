1dd11e41cf7d100ca4af20a4a3552c599686b627
Joseph <joseph@computingfacts.com>
2017-10-16 12:14:17 +0100

    updating ep-xml: 	- code refactoring 	- pom update 	- entity graph update

--- enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/config/ProteinGroupsBatchConfig.java
+++ enzyme-portal/ep-xml/src/main/java/uk/ac/ebi/ep/xml/config/ProteinGroupsBatchConfig.java
@@ -33,10 +33,10 @@ public class ProteinGroupsBatchConfig extends AbstractBatchConfig<ProteinGroups,
     private static final String READ_QUERY = "select p from ProteinGroups p";
     private static final String COUNT_QUERY = "select count(p.proteinGroupId) from ProteinGroups p";
     private static final String ROOT_TAG_NAME = "database";
-    // private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS";
+     private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS";
      
      
-      private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE ENTRY_TYPE=0";
+      //private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE ENTRY_TYPE=0";
        //private static final String NATIVE_QUERY = "SELECT * FROM PROTEIN_GROUPS WHERE ENTRY_TYPE=0 AND ROWNUM <= 10000";
      
      
