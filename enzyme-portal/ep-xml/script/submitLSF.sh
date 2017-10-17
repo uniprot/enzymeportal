75ac00d35a25af749250eaae8176b86b6b671bfd
Joseph <joseph@computingfacts.com>
2017-10-17 23:54:53 +0100

    updating ep: 	- code refactoring 	- pom update 	- config update 	- script update 	- xml transformer refactoring 	- xml validation refactoring

--- enzyme-portal/ep-xml/script/submitLSF.sh
+++ enzyme-portal/ep-xml/script/submitLSF.sh
@@ -19,6 +19,6 @@ echo "LSF logs can be found here $LOG_DIR "
 echo "(Ctrl-C to cancel, Enter to continue)"
 read ok
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 -o /ebi/uniprot/production/enzyme_portal/logs/xml/bsub-ep-xml-$(date +%Y%m%d).log ./enzyme-xml-generator.sh uzprel
-bsub -R "rusage[mem=64000]" -M 64000 -q production-rh7 -o $LOG_DIR  $1 $2
-#bsub -R "rusage[mem=128000]" -M 128000 -q production-rh7 -o $LOG_DIR  $1 $2
+#bsub -R "rusage[mem=64000]" -M 64000 -q production-rh7 -o $LOG_DIR  $1 $2
+bsub -R "rusage[mem=128000]" -M 128000 -q production-rh7 -o $LOG_DIR  $1 $2
 
