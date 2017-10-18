e7de33bb6cc049a2332c5c164f146eb68621fbab
Joseph <joseph@computingfacts.com>
2017-10-18 12:47:25 +0100

    updating ep: 	- code refactoring 	- pom update 	-hikari update (2.7.2) 	- config update

--- enzyme-portal/ep-config/src/main/java/uk/ac/ebi/ep/config/DevDataConfig.java
+++ enzyme-portal/ep-config/src/main/java/uk/ac/ebi/ep/config/DevDataConfig.java
@@ -22,9 +22,10 @@ public class DevDataConfig extends AbstractConfig {
     @Autowired
     private Environment env;
 
-    @Bean
+   // @Bean
     @Override
-    public DataSource dataSource() {
+    public DataSource driverManagerDataSource() {
+   // public DataSource dataSource() {
         String url = String.format("jdbc:oracle:thin:@%s:%s:%s",
                 env.getRequiredProperty("ep.db.host"), env.getRequiredProperty("ep.db.port"), env.getRequiredProperty("ep.db.instance"));
 
@@ -37,15 +38,17 @@ public class DevDataConfig extends AbstractConfig {
         config.setPassword(password);
         config.setDriverClassName("oracle.jdbc.OracleDriver");
 
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+//        config.addDataSourceProperty("cachePrepStmts", "true");
+//        config.addDataSourceProperty("prepStmtCacheSize", "250");
+//        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 
         return new HikariDataSource(config);
     }
 
+    @Bean
     @Override
-    public DataSource driverManagerDataSource() {
+    public DataSource dataSource() {
+   // public DataSource driverManagerDataSource() {
         String url = String.format("jdbc:oracle:thin:@%s:%s:%s",
                 env.getRequiredProperty("ep.db.host"), env.getRequiredProperty("ep.db.port"), env.getRequiredProperty("ep.db.instance"));
 
