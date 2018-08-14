724991f4f6c12e712ee8ddce3145352e30a44f4b
Joseph <joseph@computingfacts.com>
2018-08-14 15:42:52 +0100

    updated data release project: 	- pom update 	- code refactoring 	- script update

--- data-release/ep-parser/scripts/submitOneLFS.sh
+++ data-release/ep-parser/scripts/submitOneLFS.sh
@@ -5,10 +5,15 @@
 # $1: database environment (uzpdev|uzprel)
 # $2 : the script to be submitted to the farm with extension e.g chebi.sh
 
+TODAY=$(date +%Y%m%d)
+LOG_NAME=$(basename $1 .sh)
+LOG_DIR=/ebi/uniprot/production/enzyme_portal/logs/xml/bsub-$LOG_NAME-$TODAY.log
+
 echo
 echo "**************************** W A R N I N G ****************************"
 echo "THIS WILL OVERWRITE ALL DATA IN $1. ARE YOU SURE?"
 echo "(Ctrl-C to cancel, Enter to continue)"
+echo "LSF logs can be found here $LOG_DIR "
 read ok
 
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$2 $1
@@ -16,4 +21,5 @@ read ok
 #bsub $(dirname $0)/$2 $1
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 uzprel ./cofactor.sh
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 $(dirname $0)/$2 $1
-bsub -R "rusage[mem=32000]" -M 64000 -q production-rh7 $(dirname $0)/$2 $1
+#bsub -R "rusage[mem=32000]" -M 64000 -q production-rh7 -o $LOG_DIR $(dirname $0)/$2 $1
+bsub -R "rusage[mem=32000]" -M 128000 -q production-rh7 -o $LOG_DIR $(dirname $0)/$2 $1
