efed0156c82772644ed9b2e10538fb7d87eb8df1
Joseph <joseph@computingfacts.com>
2018-12-16 16:07:58 +0000

    updating xml-generator: 	- removed reaction mechanism 	- config update 	- pom update 	- test update 	- code refactoring

--- data-release/xml-generator/src/main/java/uk/ac/ebi/ep/xml/listeners/GlobalListener.java
+++ data-release/xml-generator/src/main/java/uk/ac/ebi/ep/xml/listeners/GlobalListener.java
@@ -43,7 +43,7 @@ public class GlobalListener<T, S> implements StepExecutionListener, ItemReadList
     public void afterRead(T t) {
         String timeText = printTime(readStartTime);
 
-        //log.error("Time taken to read the entry [" + t + "]: " + timeText);
+        log.error("Time taken to read the entry [" + t + "]: " + timeText);
     }
 
     @Override
