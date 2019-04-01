cd29a529ac4bad91ecf9a38618d0979e54caa76c
Joseph <joseph@computingfacts.com>
2019-04-01 16:35:51 +0100

    updating ep-parser : 	-  code refactoring - 	-  added uniqueCofactor.java 	-  added cofactorView.java 	-  compound refactoring 	- script update

--- data-release/SitemapGenerator/scripts/sitemapLSF.sh
+++ data-release/SitemapGenerator/scripts/sitemapLSF.sh
@@ -9,4 +9,4 @@ echo "This will generate sitemap at /ebi/uniprot/production/enzyme_portal/sitema
 echo "(Ctrl-C to cancel, Enter to continue)"
 read ok
 
-bsub -R "rusage[mem=32000]" -M 32000 -q production-rh6 $(dirname $0)/$1
+bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 $(dirname $0)/$1
