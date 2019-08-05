b3427e18bb7db00706e2beb482defa9120225fd6
Joseph <joseph@computingfacts.com>
2019-08-05 17:20:05 +0100

    updating ep-parser: 	- streaming chebi entities 	- code refactoring

--- data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/parsers/CompoundReactantParser.java
+++ data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/parsers/CompoundReactantParser.java
@@ -1,12 +1,11 @@
 package uk.ac.ebi.ep.parser.parsers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
+import org.springframework.util.StopWatch;
 import uk.ac.ebi.ep.centralservice.helper.MmDatabase;
 import uk.ac.ebi.ep.centralservice.helper.Relationship;
 import uk.ac.ebi.ep.model.ChebiCompound;
@@ -22,43 +21,48 @@ import uk.ac.ebi.ep.model.service.EnzymePortalParserService;
 public class CompoundReactantParser extends GenericCompound {
 
     private static final String XREF_TYPE = "CHEBI";
-    private List<LiteCompound> reactants = null;
+    //private List<LiteCompound> reactants = null;
 
     public CompoundReactantParser(EnzymePortalParserService enzymePortalParserService) {
         super(enzymePortalParserService);
-        reactants = new ArrayList<>();
+       // reactants = new ArrayList<>();
     }
 
-    private void loadCompound(LiteCompound compound) {
-
-        enzymePortalParserService.createReactant(compound.getCompoundId(), compound.getCompoundName(), compound.getCompoundSource(), compound.getRelationship(), compound.getUniprotAccession(), compound.getUrl(), compound.getCompoundRole(), compound.getNote());
-    }
+//    private void loadCompound(LiteCompound compound) {
+//        enzymePortalParserService.createReactant(compound.getCompoundId(), compound.getCompoundName(), compound.getCompoundSource(), compound.getRelationship(), compound.getUniprotAccession(), compound.getUrl(), compound.getCompoundRole(), compound.getNote());
+//    }
 
     @Transactional(readOnly = false)
     @Override
     void loadCompoundToDatabase() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         AtomicInteger counter = new AtomicInteger(1);
-        //Long total = enzymePortalParserService.countReactionInfo(XREF_TYPE);
-        Long total = enzymePortalParserService.countDistinctReactionInfoByXrefType(XREF_TYPE);
+        Long total = enzymePortalParserService.countReactionInfo(XREF_TYPE);
+        //Long total = enzymePortalParserService.countDistinctReactionInfoByXrefType(XREF_TYPE);
         log.info("About to start streaming and processing " + total + " entries.");
-        // try (Stream<EnzymeReactionInfo> reactionInfo = enzymePortalParserService.findAllReactionInfoByXrefTypeAndStream(XREF_TYPE)) {
-        try (Stream<EnzymeReactionInfo> reactionInfo = enzymePortalParserService.findUniqueXrefReactionInfoByXrefTypeAndStream(XREF_TYPE)) {
+         try (Stream<EnzymeReactionInfo> reactionInfo = enzymePortalParserService.findAllReactionInfoByXrefTypeAndStream(XREF_TYPE)) {
+        //try (Stream<EnzymeReactionInfo> reactionInfo = enzymePortalParserService.findUniqueXrefReactionInfoByXrefTypeAndStream(XREF_TYPE)) {
             reactionInfo.forEach(data -> processReactionInfo(data, counter));
 
         }
-        log.info("Writing to Enzyme Portal database... Number of reactants to write : " + reactants.size());
+       // log.info("Writing to Enzyme Portal database... Number of reactants to write : " + reactants.size());
 
-        reactants
-                .stream()
-                .filter(compound -> compound != null)
-                .forEach(compound -> loadCompound(compound));
-        log.warn("-------- Done populating the database with reactants ---------------");
-        reactants.clear();
+//        reactants
+//                .stream()
+//                .filter(compound -> compound != null)
+//                .forEach(compound -> loadCompound(compound));
+        log.warn("-------- Done populating the database with reactants ---------------"+ LocalDate.now());
+        //reactants.clear();
+        stopWatch.stop();
+     
+        log.info("Time taken to process "+ total +" entries:  "+ stopWatch.getTotalTimeSeconds() + " seconds");
 
     }
 
     private void processReactionInfo(EnzymeReactionInfo reactionInfo, AtomicInteger counter) {
-        System.out.println("Processing CHEBI ID : " + reactionInfo.getXref() + " Num processed so far : " + counter.getAndIncrement());
+       log.info("Processing CHEBI ID : " + reactionInfo.getXref() + " Num processed so far : " + counter.getAndIncrement());
+
         ChebiCompound chebiCompound = enzymePortalParserService.findChebiCompoundById(reactionInfo.getXref(), UNIPROT)
                 .stream()
                 .filter(c -> c.getSource().equalsIgnoreCase(UNIPROT))
@@ -73,21 +77,27 @@ public class CompoundReactantParser extends GenericCompound {
         if (chebiCompound != null) {
             String chebiId = chebiCompound.getChebiAccession();
             String chebiName = chebiCompound.getCompoundName();
-
-            if (chebiId != null && !blackList.contains(chebiName) && !StringUtils.isEmpty(chebiName)) {
-
-                LiteCompound entry = new LiteCompound();
-                entry.setCompoundId(chebiId);
-                entry.setCompoundName(chebiName);
-                entry.setUniprotAccession(reactionInfo.getUniprotAccession());
-                entry.setCompoundSource(MmDatabase.ChEBI.name());
-                entry.setRelationship(Relationship.is_reactant_of.name());
-                entry.setCompoundRole(Compound.Role.REACTANT.name());
-                entry.setNote(reactionInfo.getReactionDirection());//this is only specific to reactant
-                entry.setUrl("https://www.ebi.ac.uk/chebi/advancedSearchFT.do?searchString=" + chebiId);
-                reactants.add(entry);
-
+            if(chebiName == null){
+                chebiName = chebiId;
             }
+
+            //if (chebiId != null && !blackList.contains(chebiName) && !StringUtils.isEmpty(chebiName)) {
+
+//                LiteCompound entry = new LiteCompound();
+//                entry.setCompoundId(chebiId);
+//                entry.setCompoundName(chebiName);
+//                entry.setUniprotAccession(reactionInfo.getUniprotAccession());
+//                entry.setCompoundSource(MmDatabase.ChEBI.name());
+//                entry.setRelationship(Relationship.is_reactant_of.name());
+//                entry.setCompoundRole(Compound.Role.REACTANT.name());
+//                entry.setNote(reactionInfo.getReactionDirection());//this is only specific to reactant
+//                entry.setUrl("https://www.ebi.ac.uk/chebi/advancedSearchFT.do?searchString=" + chebiId);
+//                reactants.add(entry);
+                
+                enzymePortalParserService.createReactant(chebiId, chebiName, MmDatabase.ChEBI.name(),Relationship.is_reactant_of.name(), reactionInfo.getUniprotAccession(), "https://www.ebi.ac.uk/chebi/advancedSearchFT.do?searchString=" + chebiId, Compound.Role.REACTANT.name(), reactionInfo.getReactionDirection());
+    
+
+            //}
 
         }
     }
