40e9179f8ed96b41e5460350e1255469941615c8
Joseph <joseph@computingfacts.com>
2019-10-25 12:56:18 +0100

    updating enzyme portal project 	- major code deleting

--- enzyme-portal/SitemapGenerator/scripts/mvnBuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Build the SiteMapgenerator using maven 
-
-cd $(dirname $0)/..
-mvn -DskipTests=false clean package
--- enzyme-portal/ep-xml/script/checkParams.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-case $1 in
-	uzpdev|uzprel);;
-	*) echo 'runtime environment (uzpdev|uzprel) required as $1' && exit 1;;
-esac
