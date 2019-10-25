7fe7a6e67684d9cb289a7a1308de98b6389325f6
Joseph <joseph@computingfacts.com>
2019-10-25 12:30:37 +0100

    updating ep-parser: 	- log parser update 	- chebi cofactor update 	- code refactoring

--- data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/main/LogAnalyzer.java
+++ data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/main/LogAnalyzer.java
@@ -7,24 +7,29 @@ import uk.ac.ebi.ep.parser.tools.LogParser;
  * @author Joseph
  */
 public class LogAnalyzer {
-    
+
     public static void main(String[] args) {
-//        if (args == null || args.length == 0) {
-//            System.out.println("Please provide required parameters");
-//            System.exit(0);
-//
-//        }
+        if (args == null || args.length == 0) {
+            System.out.println("Please provide required parameters");
+            System.exit(0);
+
+        }
 
         // WebLogAnalyzer.analyseLog(args[0], args[1]);
+        LogParser logParser = new LogParser();
+        logParser.logReport(args[0], args[1]);
+        //test();
+    }
+
+    private static void test() {
         String userHome = System.getProperty("user.home");
-        
+
         String output = userHome + "/ep-logs/2019/";
-       //String jsonFile = "/Users/joseph/ep-logs/enzymeportal.log";
-       String jsonFile = "/Users/joseph/ep-logs/enzymeportalDev.log";
+        //String jsonFile = "/Users/joseph/ep-logs/enzymeportal.log";
+        String jsonFile = "/Users/joseph/ep-logs/enzymeportalDev.log";
         //String jsonFile = "/usr/local/apache-tomcat-9.0.14/logs/enzymeportal-json.json";
-        
+
         LogParser logParser = new LogParser();
         logParser.logReport(jsonFile, output);
-        
     }
 }
