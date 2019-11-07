fc79d4c0a0fbed12859852e6be0800b2c3c6c420
Joseph <joseph@computingfacts.com>
2019-11-07 18:46:06 +0000

    enzyme-portal update : 	- pom update 	- code refactoring 	- test update

--- data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/main/ReactantParser.java
+++ data-release/ep-parser/src/main/java/uk/ac/ebi/ep/parser/main/ReactantParser.java
@@ -28,6 +28,7 @@ public class ReactantParser {
         context.register(DevDataConfig.class);
         context.register(GlobalConfig.class);
         context.scan("uk.ac.ebi.ep.parser.config");
+         context.scan("uk.ac.ebi.ep.parser.config", "uk.ac.ebi.ep.metaboliteService");
         context.refresh();
 
         EnzymePortalCompoundParser compoundService = context.getBean(EnzymePortalCompoundParser.class);
