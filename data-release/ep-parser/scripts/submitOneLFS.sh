ecd600b5e7ab2403e6cedc64c97c874196e657f2
Joseph <joseph@computingfacts.com>
2019-11-11 17:09:32 +0000

    data-release update : 	- major code refactoring 	- pipeline update 	- parser refactoring 	- script update

--- data-release/ep-parser/scripts/submitOneLFS.sh
+++ data-release/ep-parser/scripts/submitOneLFS.sh
@@ -3,6 +3,9 @@
 #use this java setup
 export JAVA_HOME=/nfs/public/rw/webadmin/java/jdks/latest_1.8
 export PATH=$PATH:$JAVA_HOME/bin
+source ~/.bashrc
+echo $JAVA_HOME
+echo $MAVEN_HOME
 
 # e.g - how to run: sh submitOne.sh uzprel ./cofactor.sh
 # Param:
