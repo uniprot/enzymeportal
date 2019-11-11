effa885bc5d5743b6805a58c0d0ea0e737c82e3c
Joseph <joseph@computingfacts.com>
2019-11-11 17:16:13 +0000

    data-release update : 	- major code refactoring 	- pipeline update 	- parser refactoring 	- script update

--- data-release/ep-parser/scripts/checkParams.sh
+++ data-release/ep-parser/scripts/checkParams.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#use this java setup
+export JAVA_HOME=/nfs/public/rw/webadmin/java/jdks/latest_1.8
+export PATH=$PATH:$JAVA_HOME/bin
+source ~/.bashrc
+echo $JAVA_HOME
+echo $MAVEN_HOME
 case $1 in
 	uzpdev|uzprel);;
 	*) echo 'runtime environment (uzpdev|uzprel) required as $1' && exit 1;;
