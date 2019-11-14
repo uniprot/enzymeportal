d1b0d8a0fd09c0f6895a25535477e54255298106
Joseph <joseph@computingfacts.com>
2019-11-14 15:55:49 +0000

    data-release update : 	- pom update 	- script update 	- config update

--- data-release/ep-parser/scripts/submitOneLFS.sh
+++ data-release/ep-parser/scripts/submitOneLFS.sh
@@ -28,4 +28,4 @@ read ok
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 uzprel ./cofactor.sh
 #bsub -R "rusage[mem=32000]" -M 32000 -q production-rh7 $(dirname $0)/$2 $1
 #bsub -R "rusage[mem=32000]" -M 64000 -q production-rh7 -o $LOG_DIR $(dirname $0)/$2 $1
-bsub -R "rusage[mem=32000]" -M 128000 -q production-rh7 -o $LOG_DIR $(dirname $0)/$2 $1
+bsub -R "rusage[mem=32000]" -M 128000 -q production-rh74 -o $LOG_DIR $(dirname $0)/$2 $1
